45a2c1d9e8dac398241f1fa0970f939868d5ca5b
CapCap <max@aptoslabs.com>
2022-03-07 21:51:13 -0800

    Update devnet url

--- src/constants.tsx
+++ src/constants.tsx
@@ -3,8 +3,14 @@ export const testnet_url = process.env.APTOS_TESTNET_URL || "http://localhost:80
 export const networks: Record<string, string> = {
   "local": "http://localhost:8080",
   "testnet": testnet_url,
-  "dev_testnet": "https://dev.testnet.aptoslabs.com",
+  "dev_testnet": "https://dev.fullnode.aptoslabs.com",
 };
+// Remove trailing slashes
+for (let key of Object.keys(networks)) {
+  if (networks[key].endsWith("/")) {
+    networks[key] = networks[key].slice(0, -1);
+  }
+}
 
 export const default_network: string = "dev_testnet";
 
