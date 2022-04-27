959e72a16effac60f4329c1e5bc81c6d8f380ea1
Zach Denton <zach@aptoslabs.com>
2022-04-27 17:37:52 -0400

    Don't double-count pageviews.
    GA4 automatically sends a pageview event when the history state changes:
    https://support.google.com/analytics/answer/11403294?hl=en

--- src/ExplorerRoutes.tsx
+++ src/ExplorerRoutes.tsx
@@ -6,17 +6,8 @@ import ExplorerLayout from "./pages/layout";
 import TransactionPage from "./pages/Transactions/Transaction";
 import AccountPage from "./pages/Accounts/Account";
 import { TransactionsPage } from "./pages/Transactions/Transactions";
-import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga4";
-
 
 export default function ExplorerRoutes() {
-  let location = useLocation();
-
-  React.useEffect(() => {
-    ReactGA.send({hitType: "pageview", page: `${location.pathname}${location.search}`});
-  }, [location]);
-
   return (
     <ExplorerLayout>
       <Routes>
