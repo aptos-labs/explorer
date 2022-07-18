c4426835d896ca9b6a1f62e7164972523e8fd067
Zihan <zihan@aptoslabs.com>
2022-07-18 10:30:42 -0700

    [Governance] proposals table with dummy data

---
+++ src/pages/Governance/ProposalsTable.tsx
@@ -0,0 +1,198 @@
+import * as React from "react";
+import * as RRD from "react-router-dom";
+import { useTheme } from "@mui/material";
+import { Link, Box, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { Types } from "aptos";
+import {
+    renderTimestamp,
+} from "../../pages/Transactions/helpers";
+import { assertNever } from "../../utils";
+
+const TITLE_WIDTH = 400;
+const HASH_WIDTH = 300;
+
+type ProposalCellProps = {
+    proposal: any;
+};
+
+function TitleCell({ proposal }: ProposalCellProps) {
+    return (
+        <TableCell sx={{ textAlign: "left" }}>
+            <Link
+                component={RRD.Link}
+                to={`/txn/${proposal.id}`}
+                color="primary"
+            >
+                <Box
+                    component="div"
+                    sx={{
+                        width: TITLE_WIDTH,
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                    }}
+                >
+                    {proposal.title}
+                </Box>
+            </Link>
+        </TableCell >
+    );
+}
+
+function StatusCell({ proposal }: ProposalCellProps) {
+    return (
+        <TableCell sx={{ textAlign: "left" }}>
+            {proposal.status}
+        </TableCell>
+    );
+}
+
+function ProposerCell({ proposal }: ProposalCellProps) {
+    return (
+        <TableCell sx={{ textAlign: "left" }}>
+            <Box
+                component="div"
+                sx={{
+                    width: HASH_WIDTH,
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                }}
+            >
+                {proposal.proposer}
+            </Box>
+        </TableCell>
+    );
+}
+
+// TODO: make renderTimestamp a general helper and move it out of Transactions folder
+function TimestampCell({ proposal }: ProposalCellProps) {
+    const timestamp =
+        "timestamp" in proposal ? (
+            renderTimestamp(proposal.timestamp)
+        ) : (
+            <Typography variant="subtitle2" align="center">
+                -
+            </Typography>
+        );
+
+    return <TableCell sx={{ textAlign: "left" }}>{timestamp}</TableCell>;
+}
+
+const ProposalCells = Object.freeze({
+    title: TitleCell,
+    status: StatusCell,
+    proposer: ProposerCell,
+    timestamp: TimestampCell,
+});
+
+type ProposalColumn = keyof typeof ProposalCells;
+
+const DEFAULT_COLUMNS: ProposalColumn[] = [
+    "title",
+    "status",
+    "proposer",
+    "timestamp",
+];
+
+type ProposalRowProps = {
+    proposal: Types.OnChainTransaction;
+    columns: ProposalColumn[];
+};
+
+function ProposalRow({ proposal, columns }: ProposalRowProps) {
+    return (
+        <TableRow hover>
+            {columns.map((column) => {
+                const Cell = ProposalCells[column];
+                return <Cell key={column} proposal={proposal} />;
+            })}
+        </TableRow>
+    );
+}
+
+type ProposalHeaderCellProps = {
+    column: ProposalColumn;
+};
+
+function ProposalHeaderCell({ column }: ProposalHeaderCellProps) {
+    const theme = useTheme();
+    const tableCellBackgroundColor = theme.palette.background.paper;
+
+    switch (column) {
+        case "title":
+            return (
+                <TableCell
+                    sx={{
+                        textAlign: "left",
+                        width: "2%",
+                        background: `${tableCellBackgroundColor}`,
+                        borderRadius: "8px 0 0 8px",
+                    }}
+                >
+                    <Typography variant="subtitle1">Title</Typography>
+                </TableCell>
+            );
+        case "status":
+            return (
+                <TableCell
+                    sx={{ textAlign: "left", background: `${tableCellBackgroundColor}` }}
+                >
+                    <Typography variant="subtitle1">Status</Typography>
+                </TableCell>
+            );
+        case "proposer":
+            return (
+                <TableCell
+                    sx={{ textAlign: "left", background: `${tableCellBackgroundColor}` }}
+                >
+                    <Typography variant="subtitle1">Proposer</Typography>
+                </TableCell>
+            );
+        case "timestamp":
+            return (
+                <TableCell
+                    sx={{
+                        textAlign: "left",
+                        background: `${tableCellBackgroundColor}`,
+                        borderRadius: "0 8px 8px 0",
+                    }}
+                >
+                    <Typography variant="subtitle1">Timestamp</Typography>
+                </TableCell>
+            );
+        default:
+            return assertNever(column);
+    }
+}
+
+type Props = {
+    proposals: Types.OnChainTransaction[];
+    columns?: ProposalColumn[];
+};
+
+export function ProposalsTable({
+    proposals,
+    columns = DEFAULT_COLUMNS,
+}: Props) {
+    return (
+        <Table size="small">
+            <TableHead>
+                <TableRow>
+                    {columns.map((column) => (
+                        <ProposalHeaderCell key={column} column={column} />
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {proposals.map((proposal, i) => {
+                    return (
+                        <ProposalRow
+                            key={`${i}-${proposal.hash}`}
+                            proposal={proposal}
+                            columns={columns}
+                        />
+                    );
+                })}
+            </TableBody>
+        </Table>
+    );
+}
