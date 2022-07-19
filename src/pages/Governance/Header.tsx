ef2c7d7c9854ed8c201c8c26b2d9801277247331
maayan <maayansavir@gmail.com>
2022-07-18 21:30:21 -0700

    seperate elements to single components

--- src/pages/Governance/Header.tsx
+++ src/pages/Governance/Header.tsx
@@ -1,13 +1,15 @@
-import * as RRD from "react-router-dom";
-import { Box, Button, Grid, Link, Tooltip, Typography } from "@mui/material"
 import React, { useEffect } from "react"
+import { Box, Grid } from "@mui/material"
+
 import { getAptosWallet, isWalletConnected, connectToWallet, getAccountAddress } from '../../api/wallet'
+import { HeaderText } from "./HeaderText";
+import { WalletButton } from "../../components/WalletButton";
 
 export const Header = () => {
 
   const [wallet, setWallet] = React.useState<any>(null)
   const [walletIsConnected, setWalletIsConnected] = React.useState<boolean>(false)
-  const [accountAddress, setAccountAddres] = React.useState<string>("")
+  const [accountAddress, setAccountAddres] = React.useState<string | null>(null)
 
   useEffect(() => {
     setWallet(getAptosWallet())
@@ -28,27 +30,14 @@ export const Header = () => {
     <Box component="div" sx={{ p: 2, border: '1px solid gray', flexGrow: 1 }}>
       <Grid container sx={{ p: 2 }} alignItems="center" spacing={4}>
         <Grid item xs={12} sm={12} md={8} sx={{ overflow: "hidden", textOverflow: "ellipsis" }}>
-          {walletIsConnected ?
-            <Typography variant="h5" noWrap sx={{ mb: 2 }}>Hello <Link
-              component={RRD.Link}
-              to={`/account/${accountAddress}`}
-              color="primary"
-            >{accountAddress}</Link>
-            </Typography> :
-            <Typography variant="h5" sx={{ mb: 2 }}>Aptos Governance</Typography>}
-          <Typography>
-            Some instructions Some instructions Some instructions Some instructions Some instructions
-          </Typography>
+          <HeaderText walletIsConnected={walletIsConnected} accountAddress={accountAddress} />
         </Grid>
         <Grid item xs={12} sm={12} md={4} textAlign={{ sm: "left", md: "right" }}>
-          {!wallet &&
-            <Tooltip title={<Link href="https://aptos.dev/guides/building-wallet-extension" target="_blank">Please install the Aptos wallet</Link>}>
-              <span>
-                <Button variant="cta" disabled>Connect Wallet</Button>
-              </span>
-            </Tooltip>
-          }
-          {wallet && !walletIsConnected && <Button variant="cta" onClick={onConnectWalletClick}>Connect Wallet</Button>}
+          <WalletButton
+            onConnectWalletClick={onConnectWalletClick}
+            walletIsConnected={walletIsConnected}
+            wallet={wallet}
+          />
         </Grid>
       </Grid>
     </Box>
---
+++ src/pages/Governance/HeaderText.tsx
@@ -0,0 +1,32 @@
+import * as RRD from "react-router-dom";
+import React from "react"
+
+import { Link, Typography } from "@mui/material"
+
+type HeaderTextProps = {
+  walletIsConnected: boolean,
+  accountAddress: string | null
+}
+
+export const HeaderText = ({ walletIsConnected, accountAddress }: HeaderTextProps) => {
+  return (
+    <>
+      {walletIsConnected ?
+        <Typography variant="h5" noWrap sx={{ mb: 2 }}>
+          Hello {''}
+          <Link
+            component={RRD.Link}
+            to={`/account/${accountAddress}`}
+            color="primary"
+          >
+            {accountAddress}
+          </Link>
+        </Typography>
+        :
+        <Typography variant="h5" sx={{ mb: 2 }}>Aptos Governance</Typography>}
+      <Typography>
+        Some instructions Some instructions Some instructions Some instructions Some instructions
+      </Typography>
+    </>
+  )
+}
