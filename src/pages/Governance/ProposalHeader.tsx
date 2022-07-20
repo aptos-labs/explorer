fd48903b3d6184574087ba6ae2cf0040e6a41d5f
Zihan <zihan@aptoslabs.com>
2022-07-20 11:16:22 -0700

    [Gonvernance] set up proposal page components

---
+++ src/pages/Governance/ProposalHeader.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Grid } from "@mui/material"
+
+export const ProposalHeader = () => {
+    return (
+        <Grid xs={12}>
+            Header
+        </Grid >
+    )
+}
