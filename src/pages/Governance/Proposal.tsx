e767b85b7bc92217b376af9414f5cb1e7c934bc8
Maayan Savir <maayansavir@gmail.com>
2022-07-20 10:19:24 -0700

    add proposal route and page (#38)

---
+++ src/pages/Governance/Proposal.tsx
@@ -0,0 +1,10 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+export const ProposalPage = () => {
+  return (
+    <Grid xs={12} marginTop={{ md: 12, xs: 6 }}>
+      Proposal
+    </Grid >
+  )
+}
