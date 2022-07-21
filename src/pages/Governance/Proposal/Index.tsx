03346eb0689ad790aa17647c9a99bc2a27b6056f
Zihan <109111707+zihan-aptos@users.noreply.github.com>
2022-07-20 17:54:43 -0700

    [Governance] add proposal content and proposal type (#43)
    * [Governance] add proposal content and proposal type
    
    * [Governance] move proposals components to a separated folder (#42)
    
    * [Governance] move proposals components to a separated folder
    
    * address comments
    
    * [Governance] add proposal content and proposal type
    
    * address comments
    
    * address comments

--- src/pages/Governance/Proposal/Index.tsx
+++ src/pages/Governance/Proposal/Index.tsx
@@ -3,8 +3,12 @@ import React from "react";
 import { ProposalHeader } from "./Header";
 import { ProposalCard } from "./Card";
 import { ProposalContent } from "./Content";
+import { proposalsData } from "../dummyData";
 
 export const ProposalPage = () => {
+
+  // TODO: add error handling
+
   return (
     <Grid xs={12} marginTop={{ md: 12, xs: 6 }}>
       <Grid item sx={{ mb: 6 }}>
@@ -14,7 +18,7 @@ export const ProposalPage = () => {
         <ProposalCard />
       </Grid>
       <Grid item sx={{ mb: 6 }}>
-        <ProposalContent />
+        <ProposalContent proposalData={proposalsData[0]} />
       </Grid>
     </Grid >
   )
---
+++ src/pages/Governance/Types.ts
@@ -0,0 +1,19 @@
+export type ProposalType = {
+  proposal_id: string;
+  proposer: string;
+  creation_time_secs: string;
+  execution_content: {
+    code_location: string;
+    title: string;
+    description: string;
+  };
+  execution_hash: string;
+  min_vote_threshold: number;
+  expiration_secs: string;
+  early_resolution_vote_threshold: number;
+  yes_votes: number;
+  no_votes: number;
+  is_resolved: boolean;
+  is_voting_closed: boolean;
+  proposal_state: string;
+};
