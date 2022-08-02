6fc7ac2a364cb6c2c5d37397cf85a4d5949cbb54
Zihan <109111707+zihan-aptos@users.noreply.github.com>
2022-08-01 17:05:08 -0700

    [Governance] create stake page UI (#63)
    * stake page ui
    
    * stake page ui
    
    * address comments

--- src/ExplorerRoutes.tsx
+++ src/ExplorerRoutes.tsx
@@ -9,6 +9,7 @@ import {TransactionsPage} from "./pages/Transactions/Transactions";
 import {GovernancePage} from "./pages/Governance/Proposals/Index";
 import {ProposalPage} from "./pages/Governance/Proposal/Index";
 import {CreateProposalPage} from "./pages/Governance/CreateProposal/Index";
+import {StakePage} from "./pages/Governance/Stake/Index";
 
 export default function ExplorerRoutes() {
   return (
@@ -19,6 +20,7 @@ export default function ExplorerRoutes() {
         <Route path="/proposals" element={<GovernancePage />} />
         <Route path="/proposals/:handle/:id" element={<ProposalPage />} />
         <Route path="/proposals/create" element={<CreateProposalPage />} />
+        <Route path="/proposals/stake" element={<StakePage />} />
         <Route path="/txn">
           <Route path=":txnHashOrVersion" element={<TransactionPage />} />
         </Route>
---
+++ src/pages/Governance/Stake/Index.tsx
@@ -0,0 +1,99 @@
+import React, {useState} from "react";
+import {
+  Grid,
+  TextField,
+  Button,
+  OutlinedInput,
+  InputAdornment,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import {DateTimePicker} from "@mui/x-date-pickers/DateTimePicker";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+
+export function StakePage() {
+  const [stakingEndTime, setStakingEndTime] = useState<Date | null>(new Date());
+  const [stakingAmount, setStakingAmount] = useState<string>("");
+  const [operatorAddr, setOperatorAddr] = useState<string>("");
+  const [voterAddr, setVoterAddr] = useState<string>("");
+
+  const onStakingEndTimeChange = (newStakingEndTime: Date | null) => {
+    setStakingEndTime(newStakingEndTime);
+  };
+
+  const onStakingAmountChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setStakingAmount(event.target.value);
+  };
+
+  const onOperatorAddrChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOperatorAddr(event.target.value);
+  };
+
+  const onVoterAddrChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setVoterAddr(event.target.value);
+  };
+
+  const onSubmitClick = async () => {
+    // TODO: to be implemented
+    console.log("Submit Staking");
+  };
+
+  return (
+    <Grid container spacing={2} marginTop={{md: 12, xs: 6}}>
+      <Grid item xs={6} md={6}>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="outlined-adornment-amount">
+            Staking Amount
+          </InputLabel>
+          <OutlinedInput
+            label="Staking Amount"
+            value={stakingAmount}
+            onChange={onStakingAmountChange}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          />
+        </FormControl>
+      </Grid>
+      <Grid item xs={6} md={6}>
+        <FormControl fullWidth>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DateTimePicker
+              label="Staking End Time"
+              value={stakingEndTime}
+              onChange={onStakingEndTimeChange}
+              renderInput={(params) => <TextField {...params} />}
+              minTime={new Date()}
+            />
+          </LocalizationProvider>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <TextField
+          fullWidth
+          label="Operator Address"
+          variant="outlined"
+          value={operatorAddr}
+          onChange={onOperatorAddrChange}
+        />
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <TextField
+          fullWidth
+          label="Voter Address"
+          variant="outlined"
+          value={voterAddr}
+          onChange={onVoterAddrChange}
+        />
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <FormControl fullWidth>
+          <Button variant="primary" onClick={onSubmitClick}>
+            Submit
+          </Button>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
+}
