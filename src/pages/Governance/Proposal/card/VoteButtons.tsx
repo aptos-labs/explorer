e52ba31d91fb0bcdd133685bab09587e31e2498c
Zihan <zihan@aptoslabs.com>
2022-08-04 22:29:15 -0700

    code clean up

--- src/pages/Governance/Proposal/card/VoteButtons.tsx
+++ src/pages/Governance/Proposal/card/VoteButtons.tsx
@@ -5,6 +5,7 @@ import {
   Snackbar,
   Alert,
   IconButton,
+  Box,
 } from "@mui/material";
 import React, {useState} from "react";
 import useSubmitVote, {
@@ -15,6 +16,7 @@ import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import {negativeColor, voteFor, voteAgainst} from "../../constants";
 import CloseIcon from "@mui/icons-material/Close";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 // TODO:
 // 1. check if voted
@@ -45,6 +47,20 @@ export default function VoteButtons({proposalId}: Props) {
     clearVoteResponse();
   };
 
+  const voteOnSuccessSnackbarAction = (
+    <Box alignSelf="center" marginRight={1.5}>
+      <Button
+        variant="outlined"
+        color="inherit"
+        size="large"
+        onClick={refreshPage}
+        endIcon={<RefreshIcon />}
+      >
+        Refresh
+      </Button>
+    </Box>
+  );
+
   const voteOnSuccessSnackbar = voteResponse !== null && (
     <Snackbar
       open={voteResponse.succeeded}
@@ -56,17 +72,24 @@ export default function VoteButtons({proposalId}: Props) {
       <Alert
         variant="filled"
         severity="success"
-        action={
-          <Button variant="outlined" size="small" onClick={refreshPage}>
-            REFRESH
-          </Button>
-        }
+        action={voteOnSuccessSnackbarAction}
       >
         {`Vote succeeded with transaction ${
           (voteResponse as VoteResponseOnSuccess).transactionHash
         }. Please refresh to see your vote.`}
       </Alert>
     </Snackbar>
+  );
+
+  const voteOnFailureSnackbarAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={onCloseErrorAlert}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
   );
 
   const voteOnFailureSnackbar = voteResponse !== null && (
@@ -80,16 +103,7 @@ export default function VoteButtons({proposalId}: Props) {
       <Alert
         variant="filled"
         severity="error"
-        action={
-          <IconButton
-            size="small"
-            aria-label="close"
-            color="inherit"
-            onClick={onCloseErrorAlert}
-          >
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        }
+        action={voteOnFailureSnackbarAction}
       >
         {`Vote failed with error message "${
           (voteResponse as VoteResponseOnFailure).message
