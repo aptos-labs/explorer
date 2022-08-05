3a968ce87008a3d16fa66d1f878c3dfaffdbe1fe
Zihan <109111707+zihan-aptos@users.noreply.github.com>
2022-08-05 12:19:19 -0700

    [Governance] vote with connected wallet (#69)
    * vote with wallet
    
    * vote
    
    * show vote succeeded or not
    
    * nit
    
    * address comments
    
    * formatting
    
    * formatting
    
    * code clean up
    
    * comments
    
    * comments

--- src/pages/Governance/Proposal/card/VoteButtons.tsx
+++ src/pages/Governance/Proposal/card/VoteButtons.tsx
@@ -1,9 +1,22 @@
-import {Button, Stack, TextField} from "@mui/material";
+import {
+  Button,
+  Stack,
+  TextField,
+  Snackbar,
+  Alert,
+  IconButton,
+  Box,
+} from "@mui/material";
 import React, {useState} from "react";
-import useSubmitVote from "../../SubmitVote";
+import useSubmitVote, {
+  VoteResponseOnSuccess,
+  VoteResponseOnFailure,
+} from "../../hooks/useSubmitVote";
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import {negativeColor, voteFor, voteAgainst} from "../../constants";
+import CloseIcon from "@mui/icons-material/Close";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 // TODO:
 // 1. check if voted
@@ -15,7 +28,8 @@ type Props = {
 
 export default function VoteButtons({proposalId}: Props) {
   const [accountAddr, setAccountAddr] = useState<string>("");
-  const [submitVote] = useSubmitVote(proposalId);
+  const {submitVote, voteResponse, clearVoteResponse} =
+    useSubmitVote(proposalId);
 
   const onAccountAddrChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAccountAddr(event.target.value);
@@ -25,37 +39,114 @@ export default function VoteButtons({proposalId}: Props) {
     submitVote(shouldPass, accountAddr);
   };
 
-  return (
-    <Stack spacing={2}>
-      <TextField
-        fullWidth
-        label="Owner Account Address"
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  const onCloseErrorAlert = () => {
+    clearVoteResponse();
+  };
+
+  const voteOnSuccessSnackbarAction = (
+    <Box alignSelf="center" marginRight={1.5}>
+      <Button
+        variant="outlined"
+        color="inherit"
+        size="large"
+        onClick={refreshPage}
+        endIcon={<RefreshIcon />}
+      >
+        Refresh
+      </Button>
+    </Box>
+  );
+
+  const voteOnSuccessSnackbar = voteResponse !== null && (
+    <Snackbar
+      open={voteResponse.succeeded}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "center",
+      }}
+    >
+      <Alert
         variant="filled"
-        size="small"
-        margin="normal"
-        value={accountAddr}
-        onChange={onAccountAddrChange}
-      />
-      <Button
-        fullWidth
-        size="large"
-        variant="primary"
-        sx={{justifyContent: "start"}}
-        startIcon={<CheckCircleOutlinedIcon />}
-        onClick={() => onVote(true)}
+        severity="success"
+        action={voteOnSuccessSnackbarAction}
       >
-        {voteFor}
-      </Button>
-      <Button
-        fullWidth
-        size="large"
-        variant="primary"
-        sx={{justifyContent: "start", backgroundColor: negativeColor}}
-        startIcon={<CancelOutlinedIcon />}
-        onClick={() => onVote(false)}
+        {`Vote succeeded with transaction ${
+          (voteResponse as VoteResponseOnSuccess).transactionHash
+        }. Please refresh to see your vote.`}
+      </Alert>
+    </Snackbar>
+  );
+
+  const voteOnFailureSnackbarAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={onCloseErrorAlert}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
+  const voteOnFailureSnackbar = voteResponse !== null && (
+    <Snackbar
+      open={!voteResponse.succeeded}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "center",
+      }}
+    >
+      <Alert
+        variant="filled"
+        severity="error"
+        action={voteOnFailureSnackbarAction}
       >
-        {voteAgainst}
-      </Button>
-    </Stack>
+        {`Vote failed with error message "${
+          (voteResponse as VoteResponseOnFailure).message
+        }". Please try again.`}
+      </Alert>
+    </Snackbar>
+  );
+
+  return (
+    <>
+      <Stack spacing={2}>
+        <TextField
+          fullWidth
+          label="Owner Account Address"
+          variant="filled"
+          size="small"
+          margin="normal"
+          value={accountAddr}
+          onChange={onAccountAddrChange}
+        />
+        <Button
+          fullWidth
+          size="large"
+          variant="primary"
+          sx={{justifyContent: "start"}}
+          startIcon={<CheckCircleOutlinedIcon />}
+          onClick={() => onVote(true)}
+        >
+          {voteFor}
+        </Button>
+        <Button
+          fullWidth
+          size="large"
+          variant="primary"
+          sx={{justifyContent: "start", backgroundColor: negativeColor}}
+          startIcon={<CancelOutlinedIcon />}
+          onClick={() => onVote(false)}
+        >
+          {voteAgainst}
+        </Button>
+      </Stack>
+      {voteOnSuccessSnackbar}
+      {voteOnFailureSnackbar}
+    </>
   );
 }
