64993fe4b27aaebab8a1c4540023b7e3b71aef79
Zihan <109111707+zihan-aptos@users.noreply.github.com>
2022-08-08 13:54:42 -0700

    [Governance] Vote confirmation modal (#75)
    * modal
    
    * vote modal
    
    * comments addressed

--- src/pages/Governance/constants.tsx
+++ src/pages/Governance/constants.tsx
@@ -1,6 +1,17 @@
-export const primaryColor: string = "#1DE9B6"; // green
-export const negativeColor: string = "#F97373"; // red
+import {teal} from "../../themes/colors/aptosColorPalette";
+
+// TODO: add red and yellow with opacity to aptosColorPalette
+
+// teal
+export const primaryColor: string = teal[500]; // rgb(0,208,161)
+export const primaryColorOnHover: string = teal[600];
+export const primaryColorWithOpacity: string = "rgba(0,208,161,0.1)";
+// red
+export const negativeColor: string = "#F97373"; // rgb(249,115,115)
+export const negativeColorOnHover: string = "#dd6767";
+export const negativeColorWithOpacity: string = "rgba(249,115,115,0.1)";
+// yellow
 export const warningColor: string = "#D8BF45"; // yellow
 
-export const voteFor: string = "For";
-export const voteAgainst: string = "Against";
+export const voteFor: string = "FOR";
+export const voteAgainst: string = "AGAINST";
