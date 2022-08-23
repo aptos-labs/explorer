5ecc6ae492b0f093066203e3e18ec073df252753
maayan <maayansavir@gmail.com>
2022-08-23 16:07:32 -0700

    add loading icon when waiting for query response

--- src/api/hooks/useGetAccountResource.ts
+++ src/api/hooks/useGetAccountResource.ts
@@ -4,20 +4,30 @@ import {getAccountResources} from "..";
 import {ResponseError} from "../client";
 import {useGlobalState} from "../../GlobalState";
 
+type useGetAccountResourceResponse = {
+  accountResource: Types.MoveResource | undefined;
+  isLoading: boolean;
+};
+
 export function useGetAccountResource(
   address: string,
   resource: string,
-): Types.MoveResource | undefined {
+): useGetAccountResourceResponse {
   const [state, _setState] = useGlobalState();
   const accountResourcesResult = useQuery<
     Array<Types.MoveResource>,
     ResponseError
-  >(["accountResources", {address}, state.network_value], () =>
-    getAccountResources({address}, state.network_value),
+  >(
+    ["accountResources", {address}, state.network_value],
+    () => getAccountResources({address}, state.network_value),
+    {refetchOnWindowFocus: false},
   );
+
+  const {isLoading} = accountResourcesResult;
+
   const accountResource = accountResourcesResult.data?.find(
     (r) => r.type === resource,
   );
 
-  return accountResource;
+  return {accountResource, isLoading};
 }
--- src/pages/Governance/Stake/Index.tsx
+++ src/pages/Governance/Stake/Index.tsx
@@ -3,16 +3,19 @@ import React from "react";
 import {useGetAccountResource} from "../../../api/hooks/useGetAccountResource";
 import {useWalletContext} from "../../../context/wallet/context";
 import {Header} from "../components/Header";
+import LoadingModal from "../components/LoadingModal";
 
 import {Create} from "./Create";
 import {Edit} from "./Edit";
 
 export function Staking() {
   const {isConnected, accountAddress} = useWalletContext();
-  const stakePool = useGetAccountResource(
+  const {accountResource: stakePool, isLoading} = useGetAccountResource(
     accountAddress || "0x1",
     "0x1::stake::StakePool",
   );
+
+  if (isLoading) return <LoadingModal open={isLoading} />;
 
   return (
     <>
