b7b4fcae84d0a0d8ad885324a4817d7e150d671f
maayan <maayansavir@gmail.com>
2022-08-23 11:47:28 -0700

    add proposals and staking sub menus

--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -1,33 +1,49 @@
-import * as React from "react";
+import React, {useState} from "react";
+
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import {ReactComponent as HamburgerIcon} from "../assets/svg/icon_hamburger.svg";
 import {ReactComponent as CloseIcon} from "../assets/svg/icon_close.svg";
 import {grey} from "../themes/colors/aptosColorPalette";
-import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import {useTheme} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 export default function NavMobile() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const [menuAnchorEl, setMenuAnchorEl] = useState<null | HTMLElement>(null);
+  const [governanceMenuOpen, setGovernanceMenuOpen] = useState<boolean>(false);
   const theme = useTheme();
+  const navigate = useNavigate();
+
+  const menuOpen = Boolean(menuAnchorEl);
+
+  const handleGovernanceClick = (event: React.MouseEvent<HTMLElement>) => {
+    setGovernanceMenuOpen(!governanceMenuOpen);
+  };
+
+  const handleIconClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setMenuAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setMenuAnchorEl(null);
+    setGovernanceMenuOpen(false);
+  };
+
+  const handleCloseAndNavigate = (to: string) => {
+    setMenuAnchorEl(null);
+    setGovernanceMenuOpen(false);
+    navigate(to);
+  };
 
   return (
     <Box sx={{display: {xs: "block", md: "none"}}}>
       <Button
         id="nav-mobile-button"
-        aria-controls={open ? "nav-mobile-menu" : undefined}
+        aria-controls={menuOpen ? "nav-mobile-menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
+        aria-expanded={menuOpen ? "true" : undefined}
+        onClick={handleIconClick}
         sx={{
           minWidth: "0",
           width: "1.5rem",
@@ -41,12 +57,12 @@ export default function NavMobile() {
           "&[aria-expanded=true]": {opacity: "0.7"},
         }}
       >
-        {open ? <CloseIcon /> : <HamburgerIcon />}
+        {menuOpen ? <CloseIcon /> : <HamburgerIcon />}
       </Button>
       <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
+        anchorEl={menuAnchorEl}
+        open={menuOpen}
+        onClose={handleMenuClose}
         MenuListProps={{
           "aria-labelledby": "nav-mobile-button",
           sx: {
@@ -61,16 +77,26 @@ export default function NavMobile() {
           maxWidth: "none",
         }}
       >
-        <li>
-          <MenuItem onClick={handleClose} component={Link} href="/transactions">
-            Transactions
-          </MenuItem>
-        </li>
-        <li>
-          <MenuItem onClick={handleClose} component={Link} href="/proposals">
-            Governance
-          </MenuItem>
-        </li>
+        <MenuItem onClick={() => handleCloseAndNavigate("/transactions")}>
+          Transactions
+        </MenuItem>
+        <MenuItem onClick={handleGovernanceClick}>Governance</MenuItem>
+        {governanceMenuOpen && (
+          <Box
+            sx={{
+              paddingLeft: "1.5rem",
+            }}
+          >
+            <MenuItem onClick={() => handleCloseAndNavigate("/proposals")}>
+              Proposals
+            </MenuItem>
+            <MenuItem
+              onClick={() => handleCloseAndNavigate("/proposals/stake")}
+            >
+              Staking
+            </MenuItem>
+          </Box>
+        )}
       </Menu>
     </Box>
   );
