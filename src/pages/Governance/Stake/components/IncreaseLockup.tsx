85bebacc8298d2538978b11046f71eccab7e5482
maayan <maayansavir@gmail.com>
2022-08-24 10:35:26 -0700

    increase lockup button

---
+++ src/pages/Governance/Stake/components/IncreaseLockup.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {Grid, Button, FormControl, Tooltip} from "@mui/material";
+import TransactionResponseSnackbar from "../../components/snackbar/TransactionResponseSnackbar";
+import LoadingModal from "../../components/LoadingModal";
+import useSubmitIncreaseLock from "../../hooks/useSubmitIncreaseLock";
+import {renderTimestamp} from "../../../Transactions/helpers";
+
+type IncreaseLockupProps = {
+  isWalletConnected: boolean;
+  lockedUntilSec: string;
+};
+
+export function IncreaseLockup({
+  isWalletConnected,
+  lockedUntilSec,
+}: IncreaseLockupProps): JSX.Element {
+  const {
+    submitIncreaseLockup,
+    transactionInProcess,
+    transactionResponse,
+    clearTransactionResponse,
+  } = useSubmitIncreaseLock();
+
+  const onIncreaseLockupClick = async () => {
+    await submitIncreaseLockup();
+  };
+
+  const onCloseSnackbar = () => {
+    clearTransactionResponse();
+  };
+
+  const submitDisabled = !isWalletConnected;
+  const submitButton = (
+    <span>
+      <Button fullWidth variant="primary" onClick={onIncreaseLockupClick}>
+        Increase Lockup
+      </Button>
+    </span>
+  );
+
+  return (
+    <>
+      <TransactionResponseSnackbar
+        transactionResponse={transactionResponse}
+        onCloseSnackbar={onCloseSnackbar}
+      />
+      <LoadingModal open={transactionInProcess} />
+      <Grid>
+        <Grid container spacing={4}>
+          <></>
+          <Grid item xs={12} sm={8}>
+            Locked until: {renderTimestamp(lockedUntilSec)}
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <FormControl fullWidth>
+              {submitDisabled ? (
+                <Tooltip title="Please connect your wallet" arrow>
+                  {submitButton}
+                </Tooltip>
+              ) : (
+                submitButton
+              )}
+            </FormControl>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
---
+++ src/pages/Governance/hooks/useSubmitIncreaseLock.ts
@@ -0,0 +1,31 @@
+import {Types} from "aptos";
+import useSubmitTransaction from "../../../api/hooks/useSubmitTransaction";
+
+const useSubmitIncreaseLock = () => {
+  const {
+    submitTransaction,
+    transactionInProcess,
+    transactionResponse,
+    clearTransactionResponse,
+  } = useSubmitTransaction();
+
+  async function submitIncreaseLockup() {
+    const payload: Types.TransactionPayload = {
+      type: "entry_function_payload",
+      function: "0x1::stake::increase_lockup",
+      type_arguments: [],
+      arguments: [],
+    };
+
+    await submitTransaction(payload);
+  }
+
+  return {
+    submitIncreaseLockup,
+    transactionInProcess,
+    transactionResponse,
+    clearTransactionResponse,
+  };
+};
+
+export default useSubmitIncreaseLock;
