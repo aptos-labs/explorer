2aa89e78759505bcd3ea65dffce32e26d40509a9
Zihan <zihan@aptoslabs.com>
2022-08-29 16:21:12 -0700

    hot fix

--- src/pages/Governance/Stake/components/CreateOrEdit.tsx
+++ src/pages/Governance/Stake/components/CreateOrEdit.tsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from "react";
-
 import {useGetAccountResource} from "../../../../api/hooks/useGetAccountResource";
-import LoadingModal from "../../components/LoadingModal";
 import {Create} from "../Create";
 import {Edit} from "../Edit";
 
@@ -15,12 +13,10 @@ export function CreateOrEdit({
 }: CreateOrEditProps): JSX.Element {
   const [hasStakePool, setHasStakePool] = useState<boolean>(false);
 
-  const {
-    accountResource: stakePool,
-    isLoading,
-    isError,
-    refetch,
-  } = useGetAccountResource(accountAddress || "0x1", "0x1::stake::StakePool");
+  const {accountResource: stakePool, refetch} = useGetAccountResource(
+    accountAddress || "0x1",
+    "0x1::stake::StakePool",
+  );
 
   useEffect(() => {
     setHasStakePool(!!stakePool);
@@ -31,13 +27,9 @@ export function CreateOrEdit({
     refetch();
   };
 
-  if (isLoading) return <LoadingModal open={isLoading} />;
-
-  // handle errors
-  if (isError) return <div>Error</div>;
-
-  if (stakePool && hasStakePool)
+  if (stakePool && hasStakePool) {
     return <Edit stakePool={stakePool} isWalletConnected={isWalletConnected} />;
-
-  return <Create onCreateStackingPoolSuccess={onCreateStackingPoolSuccess} />;
+  } else {
+    return <Create onCreateStackingPoolSuccess={onCreateStackingPoolSuccess} />;
+  }
 }
