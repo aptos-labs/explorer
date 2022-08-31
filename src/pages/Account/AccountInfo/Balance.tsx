c2a329fc9c2488f617461c11d596d51015abd4d8
Zihan <zihan@aptoslabs.com>
2022-08-30 17:37:56 -0700

    balance

---
+++ src/pages/Account/AccountInfo/Balance.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {Typography} from "@mui/material";
+import {useGetAccountResources} from "../../../api/hooks/useGetAccountResources";
+
+interface CoinStore {
+  coin: {
+    value: string;
+  };
+}
+
+function getFormattedBalanceStr(balance: string): string {
+  return parseInt(balance).toLocaleString("en-US");
+}
+
+type BalanceProps = {
+  address: string;
+};
+
+export default function Balance({address}: BalanceProps) {
+  const {isLoading, data, error} = useGetAccountResources(address);
+
+  if (isLoading || error || !data) {
+    return null;
+  }
+
+  const coinStore = data.find(
+    (resource) =>
+      resource.type === "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>",
+  );
+
+  if (!coinStore) {
+    return null;
+  }
+
+  const coinStoreData: CoinStore = coinStore.data as CoinStore;
+  const balance = coinStoreData?.coin?.value;
+
+  return (
+    <Typography variant="body1">
+      {`Balance: ${getFormattedBalanceStr(balance)} APT`}
+    </Typography>
+  );
+}
