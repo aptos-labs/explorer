cbe5391732ee71f138fecea05e0c0fcf4464ed4f
Zihan <109111707+zihan-aptos@users.noreply.github.com>
2022-09-01 16:42:33 -0700

    fix participation (#141)

--- src/pages/Governance/hooks/useGetCoinSupplyLimit.tsx
+++ src/pages/Governance/hooks/useGetCoinSupplyLimit.tsx
@@ -1,4 +1,8 @@
 import {useGetAccountResources} from "../../../api/hooks/useGetAccountResources";
+import {GlobalState, useGlobalState} from "../../../GlobalState";
+import {getTableItem} from "../../../api";
+import {TableItemRequest} from "aptos/dist/generated";
+import {useEffect, useState} from "react";
 
 interface CoinInfo {
   decimals: number;
@@ -7,13 +11,7 @@ interface CoinInfo {
     vec: [
       {
         aggregator: {
-          vec: [
-            {
-              handle: string;
-              key: string;
-              limit: string;
-            },
-          ];
+          vec: [AggregatorData];
         };
         integer: {
           vec: [];
@@ -24,20 +22,68 @@ interface CoinInfo {
   symbol: string;
 }
 
-export function useGetCoinSupplyLimit(): string | null {
+interface AggregatorData {
+  handle: string;
+  key: string;
+  limit: string;
+}
+
+async function fetchTotalSupply(
+  aggregatorData: AggregatorData,
+  state: GlobalState,
+): Promise<number | null> {
+  const tableItemRequest = {
+    key_type: "address",
+    value_type: "u128",
+    key: aggregatorData.key,
+  } as TableItemRequest;
+
+  const supplyLimit = await getTableItem(
+    {
+      tableHandle: aggregatorData.handle,
+      data: tableItemRequest,
+    },
+    state.network_value,
+  );
+
+  // TODO: remove network check after AIT3
+  return state.network_name === "ait3"
+    ? parseInt(supplyLimit) - (Math.pow(2, 64) - 1)
+    : parseInt(supplyLimit);
+}
+
+export function useGetCoinSupplyLimit(): number | null {
+  const [state, _] = useGlobalState();
+  const [totalSupply, setTotalSupply] = useState<number | null>(null);
   const accountResourcesResult = useGetAccountResources("0x1");
 
-  if (!accountResourcesResult.data) return null;
+  if (!accountResourcesResult.data) {
+    return null;
+  }
 
   const coinInfo = accountResourcesResult.data.find(
     (resource) =>
       resource.type === "0x1::coin::CoinInfo<0x1::aptos_coin::AptosCoin>",
   );
 
-  if (coinInfo === undefined || coinInfo.data === undefined) return null;
+  if (coinInfo === undefined || coinInfo.data === undefined) {
+    return null;
+  }
 
   const coinInfoData: CoinInfo = coinInfo.data as CoinInfo;
-  const supplyLimit = coinInfoData?.supply?.vec[0]?.aggregator?.vec[0]?.limit;
+  const aggregatorData = coinInfoData?.supply?.vec[0]?.aggregator?.vec[0];
 
-  return supplyLimit === undefined ? null : supplyLimit;
+  if (aggregatorData === undefined) {
+    return null;
+  }
+
+  useEffect(() => {
+    if (aggregatorData !== undefined) {
+      fetchTotalSupply(aggregatorData, state).then((data) => {
+        data && setTotalSupply(data);
+      });
+    }
+  }, [aggregatorData]);
+
+  return totalSupply;
 }
