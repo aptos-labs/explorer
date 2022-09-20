50ab6fb1b69050ced48b0dfbbddd2f6113f08819
Zihan <zihan@aptoslabs.com>
2022-09-20 12:28:51 -0700

    token page

--- src/pages/Token/Index.tsx
+++ src/pages/Token/Index.tsx
@@ -3,16 +3,10 @@ import {gql, useQuery} from "@apollo/client";
 import {Stack, Grid} from "@mui/material";
 import React from "react";
 import HeaderSearch from "../layout/Search/Index";
-// import {BLOCK_DUMMY_DATA} from "./data";
-// import BlockTitle from "./Title";
-// import BlockTabs from "./Tabs";
+import TokenTitle from "./Title";
+import TokenTabs from "./Tabs";
 
-// creator address
-// collection name
-// token name
-// property version
-
-const MY_QUERY = gql`
+const TOKEN_QUERY = gql`
   {
     token_datas(where: {name_hash: {}, name: {_eq: "Aptos Zero: 7"}}) {
       name
@@ -41,25 +35,33 @@ const MY_QUERY = gql`
 
 export default function TokenPage() {
   const {param} = useParams();
-  const {loading, error, data} = useQuery(MY_QUERY);
-
-  console.log(data);
 
   if (typeof param !== "string") {
     return null;
   }
 
-  //   const data = BLOCK_DUMMY_DATA;
+  const {loading, error, data} = useQuery(TOKEN_QUERY);
+
+  if (loading || error || !data) {
+    // TODO: error handling
+    return null;
+  }
+
+  // TODO: add graphql data typing
+  const tokenDatas = data?.token_datas ?? [];
+  if (tokenDatas.length === 0) {
+    return null;
+  }
+  const token = tokenDatas[0];
 
   return (
     <Grid container spacing={1}>
       <HeaderSearch />
       <Grid item xs={12}>
-        token page
-        {/* <Stack direction="column" spacing={4} marginTop={2}>
-          <BlockTitle height={data.block_height} />
-          <BlockTabs data={data} />
-        </Stack> */}
+        <Stack direction="column" spacing={4} marginTop={2}>
+          <TokenTitle name={token?.name} />
+          <TokenTabs data={token} />
+        </Stack>
       </Grid>
     </Grid>
   );
