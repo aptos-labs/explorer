63ad7444309ad71478a76b11e2c976a470648b87
Zihan <zihan@aptoslabs.com>
2022-09-27 18:43:33 -0700

    blocks page

---
+++ src/api/hooks/useGetMostRecentBlocks.ts
@@ -0,0 +1,33 @@
+import {useGlobalState} from "../../GlobalState";
+import {useEffect, useState} from "react";
+import {useQuery} from "react-query";
+import {getLedgerInfo, getRecentBlocks} from "..";
+import {Types} from "aptos";
+
+export function useGetMostRecentBlocks(count: number) {
+  const [state, _] = useGlobalState();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [recentBlocks, setRecentBlocks] = useState<Types.Block[]>([]);
+
+  const {data: ledgerData} = useQuery(["ledgerInfo", state.network_value], () =>
+    getLedgerInfo(state.network_value),
+  );
+  const currentBlockHeight = ledgerData?.block_height;
+
+  useEffect(() => {
+    if (currentBlockHeight !== undefined) {
+      const fetchData = async () => {
+        const blocks = await getRecentBlocks(
+          parseInt(currentBlockHeight),
+          count,
+          state.network_value,
+        );
+        setRecentBlocks(blocks);
+        setIsLoading(false);
+      };
+      fetchData();
+    }
+  }, [currentBlockHeight, state]);
+
+  return {recentBlocks, isLoading};
+}
