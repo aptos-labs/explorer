87d79694d86b5faab3f0f0e80e7976adcc792a73
Zihan <zihan@aptoslabs.com>
2022-10-05 10:58:22 -0700

    account txns

--- src/api/hooks/useGetAccountTransactions.ts
+++ src/api/hooks/useGetAccountTransactions.ts
@@ -4,16 +4,23 @@ import {getAccountTransactions} from "..";
 import {ResponseError} from "../client";
 import {useGlobalState} from "../../GlobalState";
 
+const QUERY_LIMIT = 100;
+
 export function useGetAccountTransactions(
   address: string,
+  sequenceNumber: string | undefined,
 ): UseQueryResult<Array<Types.Transaction>, ResponseError> {
   const [state, _setState] = useGlobalState();
+
+  const sequence = sequenceNumber === undefined ? 0 : parseInt(sequenceNumber);
+  const limit = sequence < QUERY_LIMIT ? undefined : QUERY_LIMIT;
+  const start = sequence < QUERY_LIMIT ? undefined : sequence - QUERY_LIMIT;
 
   const accountTransactionsResult = useQuery<
     Array<Types.Transaction>,
     ResponseError
-  >(["accountTransactions", {address}, state.network_value], () =>
-    getAccountTransactions({address, limit:1000}, state.network_value),
+  >(["accountTransactions", {address,start, limit}, state.network_value], () =>
+    getAccountTransactions({address, start, limit}, state.network_value),
   );
 
   return accountTransactionsResult;
--- src/pages/Account/Tabs/TransactionsTab.tsx
+++ src/pages/Account/Tabs/TransactionsTab.tsx
@@ -3,13 +3,28 @@ import {Alert} from "@mui/material";
 import TransactionsTable from "../../Transactions/TransactionsTable";
 import Error from "../Error";
 import {useGetAccountTransactions} from "../../../api/hooks/useGetAccountTransactions";
+import {useQuery} from "react-query";
+import {Types} from "aptos";
+import {ResponseError} from "../../../api/client";
+import {useGlobalState} from "../../../GlobalState";
+import {getAccount} from "../../../api";
 
 type TransactionsTabProps = {
   address: string;
 };
 
 export default function TransactionsTab({address}: TransactionsTabProps) {
-  const {isLoading, data, error} = useGetAccountTransactions(address);
+  const [state, _] = useGlobalState();
+
+  const {data: accountData} = useQuery<Types.AccountData, ResponseError>(
+    ["account", {address}, state.network_value],
+    () => getAccount({address}, state.network_value),
+  );
+
+  const {isLoading, data, error} = useGetAccountTransactions(
+    address,
+    accountData?.sequence_number,
+  );
 
   if (isLoading) {
     return null;
