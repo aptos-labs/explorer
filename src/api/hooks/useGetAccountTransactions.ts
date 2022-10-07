1de52ad740b9734085876df4f92b87975fff5065
Zihan <109111707+zihan-aptos@users.noreply.github.com>
2022-10-07 13:19:17 -0700

    [Explorer] account transactions pagination (#203)
    * account transactions pagination
    
    * rebase pagination
    
    * rebase
    
    * rebase

--- src/api/hooks/useGetAccountTransactions.ts
+++ src/api/hooks/useGetAccountTransactions.ts
@@ -6,14 +6,16 @@ import {useGlobalState} from "../../GlobalState";
 
 export function useGetAccountTransactions(
   address: string,
+  start?: number,
+  limit?: number,
 ): UseQueryResult<Array<Types.Transaction>, ResponseError> {
   const [state, _setState] = useGlobalState();
 
   const accountTransactionsResult = useQuery<
     Array<Types.Transaction>,
     ResponseError
-  >(["accountTransactions", {address}, state.network_value], () =>
-    getAccountTransactions({address}, state.network_value),
+  >(["accountTransactions", {address, start, limit}, state.network_value], () =>
+    getAccountTransactions({address, start, limit}, state.network_value),
   );
 
   return accountTransactionsResult;
