8e85a60c133c5bfa8825084da45d07825c9dec40
Zihan <zihan@aptoslabs.com>
2022-10-20 13:56:25 -0700

    display transaction fucntion

--- src/pages/Transaction/helpers.tsx
+++ src/pages/Transaction/helpers.tsx
@@ -38,7 +38,7 @@ export function getLearnMoreTooltip(txnField: string): JSX.Element | null {
       );
     case "function":
       return (
-        <LearnMoreTooltip text="Transaction function. You can also find it in Payload." />
+        <LearnMoreTooltip text="Move function executed in the transaction." />
       );
     case "gas_fee":
       return (
