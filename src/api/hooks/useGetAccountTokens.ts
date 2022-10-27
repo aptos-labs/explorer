27fc335709a4d8a942f65ab84c7c82c0646207b0
Zihan <zihan@aptoslabs.com>
2022-10-27 14:03:01 -0700

    account tokens pagination

---
+++ src/api/hooks/useGetAccountTokens.ts
@@ -0,0 +1,70 @@
+import {gql, useQuery as useGraphqlQuery} from "@apollo/client";
+
+// const ACCOUNT_TRANSACTIONS_COUNT_QUERY = gql`
+//   query AccountTransactionsCount($address: String) {
+//     move_resources_aggregate(
+//       where: {address: {_eq: $address}}
+//       distinct_on: transaction_version
+//     ) {
+//       aggregate {
+//         count
+//       }
+//     }
+//   }
+// `;
+
+// export function useGetAccountAllTransactionCount(
+//   address: string,
+// ): number | undefined {
+//   // whenever talking to the indexer, the address needs to fill in leading 0s
+//   // for example: 0x123 => 0x000...000123  (61 0s before 123)
+//   const addr64Hash = "0x" + address.substring(2).padStart(64, "0");
+
+//   const {loading, error, data} = useGraphqlQuery(
+//     ACCOUNT_TRANSACTIONS_COUNT_QUERY,
+//     {variables: {address: addr64Hash}},
+//   );
+
+//   if (loading || error || !data) {
+//     return undefined;
+//   }
+
+//   return data.move_resources_aggregate?.aggregate?.count;
+// }
+
+const TOKENS_QUERY = gql`
+  query TokensData($owner_address: String, $limit: Int, $offset: Int) {
+    current_token_ownerships(
+      where: {owner_address: {_eq: $owner_address}, amount: {_gt: "0"}}
+      limit: $limit
+      offset: $offset
+    ) {
+      token_data_id_hash
+      name
+      collection_name
+      table_type
+      property_version
+      amount
+    }
+  }
+`;
+
+export function useGetAccountTokens(
+  address: string,
+  limit: number,
+  offset?: number,
+) {
+  // whenever talking to the indexer, the address needs to fill in leading 0s
+  // for example: 0x123 => 0x000...000123  (61 0s before 123)
+  const addr64Hash = "0x" + address.substring(2).padStart(64, "0");
+
+  const {loading, error, data} = useGraphqlQuery(TOKENS_QUERY, {
+    variables: {owner_address: addr64Hash, limit: limit, offset: offset},
+  });
+
+  if (loading || error || !data) {
+    return [];
+  }
+
+  return data?.current_token_ownerships ?? [];
+}
