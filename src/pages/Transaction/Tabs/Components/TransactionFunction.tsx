b1c8544ec04c57058fd98fd2c094c8ba71c2f6a1
Zihan <zihan@aptoslabs.com>
2022-11-08 17:27:17 -0800

    show script in function section

--- src/pages/Transaction/Tabs/Components/TransactionFunction.tsx
+++ src/pages/Transaction/Tabs/Components/TransactionFunction.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Box, Stack, SxProps, Theme, useTheme} from "@mui/material";
 import {Types} from "aptos";
 import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
 const TEXT_COLOR_LIGHT = "#0EA5E9";
 const TEXT_COLOR_DARK = "#83CCED";
@@ -64,6 +65,17 @@ function CoinTransferCodeLine({sx}: {sx?: SxProps<Theme>}): JSX.Element {
   );
 }
 
+function ScriptCodeLine({sx}: {sx?: SxProps<Theme>}): JSX.Element {
+  return (
+    <CodeLineBox sx={[...(Array.isArray(sx) ? sx : [sx])]}>
+      <Stack direction="row" alignItems="center" spacing={1.5}>
+        <DescriptionOutlinedIcon sx={{fontSize: 17, padding: 0}} />
+        <Box>{`Script`}</Box>
+      </Stack>
+    </CodeLineBox>
+  );
+}
+
 export default function TransactionFunction({
   transaction,
   sx,
@@ -71,7 +83,15 @@ export default function TransactionFunction({
   transaction: Types.Transaction;
   sx?: SxProps<Theme>;
 }) {
-  if (!("payload" in transaction) || !("function" in transaction.payload)) {
+  if (!("payload" in transaction)) {
+    return null;
+  }
+
+  if (transaction.payload.type === "script_payload") {
+    return <ScriptCodeLine sx={[...(Array.isArray(sx) ? sx : [sx])]} />;
+  }
+
+  if (!("function" in transaction.payload)) {
     return null;
   }
 
