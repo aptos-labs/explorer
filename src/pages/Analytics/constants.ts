f030d54f4316d8feeefb37f8c6b797afa6cea886
Zihan <zihan@aptoslabs.com>
2022-12-12 11:21:38 -0800

    [Analytics] chart dot styling

--- src/pages/Analytics/constants.ts
+++ src/pages/Analytics/constants.ts
@@ -1,2 +1,3 @@
-export const COLOR = "rgb(129, 140, 248, 0.6)";
-export const BACKGROUND_COLOR = "rgb(129, 140, 248, 0.4)";
+export const COLOR = "rgba(129, 140, 248, 0.6)";
+export const BACKGROUND_COLOR = "rgba(129, 140, 248, 0.4)";
+export const HIGHLIGHT_COLOR = "#A5B4FC";
