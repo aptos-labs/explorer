ae93fd56d4618330be7a545c523026218241b366
jillxuu <jill@aptoslabs.com>
2023-01-05 11:30:50 -0800

    [explorer] add skeleton loading on validator page

---
+++ src/pages/Validators/Components/NodeCounts.tsx
@@ -0,0 +1,60 @@
+import {Skeleton, Typography} from "@mui/material";
+import React from "react";
+import {useGetValidatorSet} from "../../../api/hooks/useGetValidatorSet";
+import {ValidatorGeoMetric} from "../../../api/hooks/useGetValidatorSetGeoData";
+import {grey} from "../../../themes/colors/aptosColorPalette";
+import {
+  fontSizeBodySmall,
+  fontSizeSubtitle,
+  fontSizeTitle,
+  fontSizeTitleSmall,
+} from "../constants";
+import MetricSection from "./MetricSection";
+
+type NodeCountsProps = {
+  validatorGeoMetric: ValidatorGeoMetric;
+  isSkeletonLoading: boolean;
+};
+
+export default function NodeCounts({
+  validatorGeoMetric,
+  isSkeletonLoading,
+}: NodeCountsProps) {
+  const {numberOfActiveValidators} = useGetValidatorSet();
+
+  return !isSkeletonLoading ? (
+    <MetricSection>
+      <Typography sx={{fontSize: {xs: fontSizeTitleSmall, md: fontSizeTitle}}}>
+        {numberOfActiveValidators} Nodes
+      </Typography>
+      <Typography
+        sx={{fontSize: {xs: fontSizeBodySmall, md: fontSizeSubtitle}}}
+        color={grey[450]}
+      >
+        {validatorGeoMetric.countryCount} Countries
+      </Typography>
+      <Typography
+        sx={{fontSize: {xs: fontSizeBodySmall, md: fontSizeSubtitle}}}
+        color={grey[450]}
+      >
+        {validatorGeoMetric.cityCount} Cities
+      </Typography>
+    </MetricSection>
+  ) : (
+    <MetricSection>
+      <Typography sx={{fontSize: {xs: fontSizeTitleSmall, md: fontSizeTitle}}}>
+        <Skeleton width={150} />
+      </Typography>
+      <Typography
+        sx={{fontSize: {xs: fontSizeBodySmall, md: fontSizeSubtitle}}}
+      >
+        <Skeleton width={140} />
+      </Typography>
+      <Typography
+        sx={{fontSize: {xs: fontSizeBodySmall, md: fontSizeSubtitle}}}
+      >
+        <Skeleton width={130} />
+      </Typography>
+    </MetricSection>
+  );
+}
