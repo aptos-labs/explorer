4a61b5b6425ae681f8afd6a7bc9adf405f3203b7
jillxuu <jill@aptoslabs.com>
2023-01-24 20:40:20 -0800

    address comments

--- src/pages/DelegatoryValidator/StakeDialog.tsx
+++ src/pages/DelegatoryValidator/StakeDialog.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   Dialog,
   DialogActions,
@@ -12,13 +11,14 @@ import {
   ListItem,
   ListItemText,
   OutlinedInput,
+  Stack,
   Typography,
 } from "@mui/material";
 import React, {useState} from "react";
 import CloseIcon from "@mui/icons-material/Close";
-import {grey} from "../../themes/colors/aptosColorPalette";
 import {Types} from "aptos";
 import TimestampValue from "../../components/IndividualPageContent/ContentValue/TimestampValue";
+import {grey} from "../../themes/colors/aptosColorPalette";
 
 type StakeDialogProps = {
   handleDialogClose: () => void;
@@ -46,89 +46,72 @@ export default function StakeDialog({
 
   return (
     <Dialog open={isDialogOpen} onClose={handleDialogClose}>
-      <Box sx={{marginX: 4}}>
-        <DialogContent>
-          <IconButton
-            onClick={handleDialogClose}
-            sx={{
-              position: "absolute",
-              right: 12,
-              top: 12,
-              color: (theme) => theme.palette.grey[500],
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </DialogContent>
+      <IconButton
+        onClick={handleDialogClose}
+        sx={{
+          position: "absolute",
+          right: 12,
+          top: 12,
+          color: (theme) => theme.palette.grey[500],
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <Stack sx={{marginX: 4, marginY: 4}}>
         <DialogTitle>
           <Typography variant="h5" textAlign="center">
             Stake Into The Pool
           </Typography>
         </DialogTitle>
         <DialogContent>
-          <FormHelperText
-            sx={{
-              fontSize: "1rem",
-              lineHeight: "1.1rem",
-            }}
-          >
-            Enter amount
-          </FormHelperText>
-          <OutlinedInput
-            onChange={handleStakedAmountChange}
-            value={stakedAmount}
-            sx={{marginTop: 2}}
-            fullWidth
-            placeholder="0"
-            endAdornment={
-              <InputAdornment
-                position="end"
-                sx={{ml: 0.5, marginTop: "0!important"}}
-              >
-                APT
-              </InputAdornment>
-            }
-          />
-          {stakedAmountError && <FormHelperText error>Error</FormHelperText>}
-        </DialogContent>
-        <DialogContent>
-          <List dense>
-            <ListItem>
-              <ListItemText primary="Operator Commission" />
-              <Typography>N/A</Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Compound Rewards" />
-              <Typography>N/A</Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Next Unlock In" />
-              <Typography>
-                {<TimestampValue timestamp={lockedUntilSecs?.toString()!} />}
-              </Typography>
-            </ListItem>
-          </List>
-        </DialogContent>
-        <DialogContent>
-          <DialogActions>
-            <Button
-              onClick={handleDialogClose}
-              variant="primary"
+          <Stack direction="column" spacing={2}>
+            <FormHelperText sx={{fontSize: "1rem"}}>
+              Enter amount
+            </FormHelperText>
+            <OutlinedInput
+              onChange={handleStakedAmountChange}
+              value={stakedAmount}
               fullWidth
-              disabled={stakedAmountError || !stakedAmount}
-            >
-              Deposit
-            </Button>
-          </DialogActions>
-          <Typography
-            fontSize={10}
-            sx={{color: grey[450], textAlign: "center", marginTop: 2}}
+              placeholder="0"
+              endAdornment={<InputAdornment position="end">APT</InputAdornment>}
+            />
+            {stakedAmountError && <FormHelperText error>Error</FormHelperText>}
+            <List dense>
+              <ListItem>
+                <ListItemText primary="Operator Commission" />
+                <Typography>N/A</Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Compound Rewards" />
+                <Typography>N/A</Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Next Unlock In" />
+                <Typography>
+                  {<TimestampValue timestamp={lockedUntilSecs?.toString()!} />}
+                </Typography>
+              </ListItem>
+            </List>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleDialogClose}
+            variant="primary"
+            fullWidth
+            disabled={stakedAmountError || !stakedAmount}
+            sx={{marginX: 2}}
           >
+            Deposit
+          </Button>
+        </DialogActions>
+        <DialogContent sx={{textAlign: "center"}}>
+          <Typography variant="caption" color={grey[450]}>
             Be aware that you will be able to see your funds in the pool after 1
             epoch (~2 hours) due to the delay time
           </Typography>
         </DialogContent>
-      </Box>
+      </Stack>
     </Dialog>
   );
 }
