293376ee6b9b09977ef4f3b507c439819326b959
jillxuu <jill@aptoslabs.com>
2023-03-03 10:59:25 -0800

    [analytics] add wallet connection and search stats events to GA using GTM

---
+++ src/api/hooks/useGoogleTagManager.ts
@@ -0,0 +1,17 @@
+import TagManager from "react-gtm-module";
+
+type GTMParams = {
+  events: object;
+};
+
+export const initGTM = ({events}: GTMParams) => {
+  const tagManagerArgs = {
+    gtmId: process.env.GTM_ID || "GTM-ND9VTF4",
+    events,
+  };
+  TagManager.initialize(tagManagerArgs);
+};
+
+export const sendToGTM = (dataLayer: Object): void => {
+  TagManager.dataLayer(dataLayer);
+};
