46333dc7d62e8d4849cf98fcfdfa1d55b8e915ab
jill <121921928+jillxuu@users.noreply.github.com>
2023-03-16 14:35:21 -0700

    [delegation] update banner with new design (#401)
    * [delegation] update banner with new design
    
    * address comments

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
@@ -7,31 +7,12 @@ import Footer from "./Footer";
 import {GlobalStateProvider} from "../../GlobalState";
 import {ProvideColorMode} from "../../context";
 import {GraphqlClientProvider} from "../../api/hooks/useGraphqlClient";
-import {
-  Alert,
-  Collapse,
-  Divider,
-  IconButton,
-  Link,
-  Stack,
-  Typography,
-  useTheme,
-} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import {primary} from "../../themes/colors/aptosColorPalette";
-import {useGetInDevMode} from "../../api/hooks/useGetInDevMode";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function ExplorerLayout({children}: LayoutProps) {
-  const theme = useTheme();
-  const inDev = useGetInDevMode();
-  const [bannerOpen, setBannerOpen] = useState<boolean>(true);
-  // TODO(jill): update learn more link
-
   return (
     <ProvideColorMode>
       <CssBaseline />
@@ -48,49 +29,6 @@ export default function ExplorerLayout({children}: LayoutProps) {
             }}
           >
             <Header />
-            {inDev && (
-              <Collapse in={bannerOpen}>
-                <Alert
-                  sx={{
-                    backgroundColor:
-                      theme.palette.mode === "dark" ? "#1E40AF" : "#1D4ED8",
-                    borderRadius: 0,
-                  }}
-                  icon={false}
-                  action={
-                    <Stack direction="row" spacing={3} color={primary[400]}>
-                      <Link href={""} color={primary[400]}>
-                        <Stack direction="row" paddingTop={1} spacing={1}>
-                          <Typography>LEARN MORE</Typography>
-                          <ArrowForwardIosIcon />
-                        </Stack>
-                      </Link>
-                      <Divider
-                        orientation="vertical"
-                        flexItem
-                        variant="middle"
-                        sx={{backgroundColor: "#3B82F6"}}
-                      />
-
-                      <IconButton
-                        aria-label="close"
-                        color="inherit"
-                        size="medium"
-                        onClick={() => {
-                          setBannerOpen(false);
-                        }}
-                      >
-                        <CloseIcon fontSize="inherit" />
-                      </IconButton>
-                    </Stack>
-                  }
-                >
-                  <Typography color="#ffffff">
-                    Power Aptos network. Stake tokens and earn rewards.
-                  </Typography>
-                </Alert>
-              </Collapse>
-            )}
             <Container maxWidth="xl" sx={{flexGrow: 4, paddingTop: "2rem"}}>
               {children}
             </Container>
