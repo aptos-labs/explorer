40ccd3c766d6aee661359589efc0a4ba260d0571
jillxuu <jill@aptoslabs.com>
2023-03-17 14:07:42 -0700

    [delegation] add mobile view to banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,6 +9,8 @@ import {
   Box,
   SxProps,
   Theme,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import React, {useState} from "react";
 import CloseIcon from "@mui/icons-material/Close";
@@ -24,80 +26,131 @@ interface BannerProps {
 
 export function Banner({children, handleClick, sx}: BannerProps) {
   const [bannerOpen, setBannerOpen] = useState<boolean>(true);
+  const theme = useTheme();
+  const isOnMobile = !useMediaQuery(theme.breakpoints.up("md"));
+
+  const learnMoreButton = (
+    <Button variant="text" onClick={handleClick} sx={{alignSelf: "flex-start"}}>
+      <Typography>LEARN MORE</Typography>
+      <ArrowForwardIosIcon sx={{marginLeft: 2}} fontSize="small" />
+    </Button>
+  );
+
+  const divider = (
+    <Divider
+      orientation="vertical"
+      variant="middle"
+      flexItem
+      sx={{color: grey[200]}}
+    />
+  );
+
+  const closeIcon = (
+    <IconButton
+      sx={{
+        color: "#ffffff",
+        position: "relative",
+        top: 0,
+        right: 0,
+      }}
+      size="medium"
+      onClick={() => {
+        setBannerOpen(false);
+      }}
+    >
+      <CloseIcon fontSize="inherit" />
+    </IconButton>
+  );
+
+  const icon = (
+    <Typography
+      sx={{
+        backgroundColor: "#8B5CF6",
+        color: "#ffffff",
+        borderRadius: 1,
+        paddingX: 1,
+        width: "3rem",
+        minWidth: "3rem",
+        height: "1.5rem",
+      }}
+    >
+      NEW
+    </Typography>
+  );
+
+  const text = (
+    <Typography
+      color="#ffffff"
+      sx={{
+        fontFamily: "apparat-semicond,Geneva,Tahoma,Verdana,sans-serif",
+      }}
+    >
+      {children}
+    </Typography>
+  );
 
   return (
     <Collapse in={bannerOpen}>
-      <Box
-        sx={[
-          {
-            textOverflow: "ellipsis",
-            overflow: "hidden",
-            whiteSpace: "nowrap",
-          },
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-      >
-        <Alert
-          sx={{backgroundImage: `url(${AptosBannerImage})`, borderRadius: 1}}
-          icon={false}
-          action={
+      <Box sx={[...(Array.isArray(sx) ? sx : [sx])]}>
+        {isOnMobile ? (
+          <Alert
+            sx={{backgroundImage: `url(${AptosBannerImage})`, borderRadius: 1}}
+            icon={false}
+            action={
+              <Stack
+                direction="column"
+                spacing={1}
+                marginRight={2}
+                sx={{verticalAlign: "center"}}
+              >
+                {closeIcon}
+              </Stack>
+            }
+          >
+            <Stack
+              direction="column"
+              spacing={1}
+              marginLeft={5}
+              sx={{
+                paddingTop: 0.5,
+              }}
+            >
+              {icon}
+              {text}
+              {learnMoreButton}
+            </Stack>
+          </Alert>
+        ) : (
+          <Alert
+            sx={{backgroundImage: `url(${AptosBannerImage})`, borderRadius: 1}}
+            icon={false}
+            action={
+              <Stack
+                direction="row"
+                spacing={1}
+                marginRight={2}
+                sx={{verticalAlign: "center"}}
+              >
+                {learnMoreButton}
+                {divider}
+                {closeIcon}
+              </Stack>
+            }
+          >
             <Stack
               direction="row"
               spacing={1}
-              marginRight={2}
-              sx={{verticalAlign: "center"}}
+              marginLeft={5}
+              sx={{
+                paddingTop: 0.5,
+                verticalAlign: "center",
+              }}
             >
-              <Button variant="text" onClick={handleClick}>
-                <Typography>LEARN MORE</Typography>
-                <ArrowForwardIosIcon sx={{marginLeft: 2}} fontSize="small" />
-              </Button>
-              <Divider
-                orientation="vertical"
-                variant="middle"
-                flexItem
-                sx={{color: grey[200]}}
-              />
-              <IconButton
-                sx={{color: "#ffffff"}}
-                size="medium"
-                onClick={() => {
-                  setBannerOpen(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
+              {icon}
+              {text}
             </Stack>
-          }
-        >
-          <Stack
-            direction="row"
-            spacing={1}
-            marginLeft={5}
-            sx={{
-              paddingTop: 0.5,
-              verticalAlign: "center",
-            }}
-          >
-            <Typography
-              sx={{
-                backgroundColor: "#8B5CF6",
-                color: "#ffffff",
-                borderRadius: 1,
-                paddingX: 1,
-              }}
-            >
-              NEW
-            </Typography>
-            <Typography
-              color="#ffffff"
-              sx={{
-                fontFamily: "apparat-semicond,Geneva,Tahoma,Verdana,sans-serif",
-              }}
-            >
-              {children}
-            </Typography>
-          </Stack>
-        </Alert>
+          </Alert>
+        )}
       </Box>
     </Collapse>
   );
