c74875317b762cace0a1df40333fee1f416107cd
jillxuu <jill@aptoslabs.com>
2023-03-17 12:22:08 -0700

    [delegation] add side panel

--- src/pages/Validators/StakingBanner.tsx
+++ src/pages/Validators/StakingBanner.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import {useGetInDevMode} from "../../api/hooks/useGetInDevMode";
 import {Banner} from "../../components/Banner";
+import {StakingDrawer} from "./StakingDrawer";
 
 export function StakingBanner() {
   const inDev = useGetInDevMode();
+  const [open, setOpen] = useState<boolean>(false);
 
   const handleClick = () => {
-    // TODO: will add the "open side panel" action here
+    setOpen(!open);
   };
 
   return inDev ? (
-    <Banner handleClick={handleClick} sx={{marginBottom: 2}}>
-      Aptos enables delegations and staking services. See Staking for more
-      details.
-    </Banner>
+    <>
+      <Banner handleClick={handleClick} sx={{marginBottom: 2}}>
+        Aptos enables delegations and staking services. See Staking for more
+        details.
+      </Banner>
+      <StakingDrawer open={open} handleClick={handleClick} />
+    </>
   ) : null;
 }
