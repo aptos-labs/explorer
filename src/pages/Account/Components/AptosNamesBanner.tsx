fffd026606252c63d43fac73d603c7c704f257c6
jill <121921928+jillxuu@users.noreply.github.com>
2023-04-05 14:24:39 -0700

    [delegation] add mobile view to banner (#408)
    * [delegation] add mobile view to banner
    
    * update fontfamily
    
    * address comments

--- src/pages/Account/Components/AptosNamesBanner.tsx
+++ src/pages/Account/Components/AptosNamesBanner.tsx
@@ -5,12 +5,16 @@ import {useGetInDevMode} from "../../../api/hooks/useGetInDevMode";
 import {Banner} from "../../../components/Banner";
 import useSubmitTransaction from "../../../api/hooks/useSubmitTransaction";
 import {useWallet} from "@aptos-labs/wallet-adapter-react";
+import {Stack, useMediaQuery, useTheme} from "@mui/material";
 
 export function AptosNamesBanner() {
   const inDev = useGetInDevMode();
   const {connected} = useWallet();
   const {submitTransaction} = useSubmitTransaction();
-  const action = (
+  const theme = useTheme();
+  const isOnMobile = !useMediaQuery(theme.breakpoints.up("md"));
+
+  const ANSConnector = (
     <AptosNamesConnector
       buttonLabel="Claim your name"
       onSignTransaction={submitTransaction}
@@ -19,9 +23,20 @@ export function AptosNamesBanner() {
     />
   );
 
+  const action = isOnMobile ? null : ANSConnector;
+  const text = "Claim your ANS name today!";
+  const children = isOnMobile ? (
+    <Stack direction="column">
+      {text}
+      {ANSConnector}
+    </Stack>
+  ) : (
+    <>{text}</>
+  );
+
   return inDev ? (
     <Banner action={action} sx={{marginBottom: 2}}>
-      Claim your ANS name today!
+      {children}
     </Banner>
   ) : null;
 }
