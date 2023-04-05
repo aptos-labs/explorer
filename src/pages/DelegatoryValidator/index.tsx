1d9f518cab536333e0a09bd85d07643916c18a95
jill <121921928+jillxuu@users.noreply.github.com>
2023-04-05 16:07:04 -0700

    [delegation] add completely inactive delegation validators (#443)

--- src/pages/DelegatoryValidator/index.tsx
+++ src/pages/DelegatoryValidator/index.tsx
@@ -6,13 +6,18 @@ import ValidatorTitle from "./Title";
 import ValidatorDetailCard from "./DetailCard";
 import ValidatorStakingBar from "./StakingBar";
 import {HexString} from "aptos";
-import {useGetValidators} from "../../api/hooks/useGetValidators";
+import {
+  useGetValidators,
+  ValidatorData,
+} from "../../api/hooks/useGetValidators";
 import MyDepositsSection from "./MyDepositsSection";
 import {useGetAccountResource} from "../../api/hooks/useGetAccountResource";
 import {useWallet} from "@aptos-labs/wallet-adapter-react";
 import {SkeletonTheme} from "react-loading-skeleton";
 import {useGetValidatorPageSkeletonLoading} from "../../api/hooks/useGetValidatorPageSkeletonLoading";
 import {DelegationStateContext} from "./context/DelegationContext";
+import {useGetDelegatedStakingPoolList} from "../../api/hooks/useGetDelegatedStakingPoolList";
+import {useEffect, useState} from "react";
 
 export default function ValidatorPage() {
   const address = useParams().address ?? "";
@@ -29,9 +34,43 @@ export default function ValidatorPage() {
     isSkeletonLoading,
   } = useGetValidatorPageSkeletonLoading();
 
-  const validator = validators.find(
-    (validator) => validator.owner_address === addressHex.hex(),
-  );
+  const {delegatedStakingPools, loading} =
+    useGetDelegatedStakingPoolList() ?? [];
+
+  const [validator, setValidator] = useState<ValidatorData>();
+
+  useEffect(() => {
+    let validator = validators.find(
+      (validator) => validator.owner_address === addressHex.hex(),
+    );
+    if (!loading && !validator) {
+      // If the validator is not in the list of validators, it means that the validator was never active.
+      // In this case, we need to get the validator data from the delegated staking pools list and manually
+      // populate required fields.
+      const delegatedStakingPoolsNotInValidators: ValidatorData[] =
+        delegatedStakingPools
+          .filter((pool) => {
+            return addressHex.hex() === pool.staking_pool_address;
+          })
+          .map((pool) => ({
+            owner_address: pool.staking_pool_address,
+            operator_address: pool.current_staking_pool.operator_address,
+            voting_power: "0",
+            governance_voting_record: "",
+            last_epoch: 0,
+            last_epoch_performance: "",
+            liveness: 0,
+            rewards_growth: 0,
+            apt_rewards_distributed: 0,
+          }));
+
+      setValidator(
+        delegatedStakingPoolsNotInValidators.length > 0
+          ? delegatedStakingPoolsNotInValidators[0]
+          : undefined,
+      );
+    }
+  }, [delegatedStakingPools, loading, validators]);
 
   if (!validator || !accountResource) {
     return null;
