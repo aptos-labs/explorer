1849f0c43f62fbffc3fd3d81c4394d71145a29ef
QZQ <928221136@qq.com>
2023-05-31 08:11:33 +0800

    Make contract feature prod (#548)

--- src/pages/Account/Tabs/ModulesTab/index.tsx
+++ src/pages/Account/Tabs/ModulesTab/index.tsx
@@ -1,19 +1,9 @@
-import {Box} from "@mui/material";
-import React from "react";
-import {useGlobalState} from "../../../../global-config/GlobalConfig";
 import ModulesTabs from "./Tabs";
-import ViewCode from "./ViewCode";
 
 type ModulesTabProps = {
   address: string;
 };
 
 export default function ModulesTab({address}: ModulesTabProps) {
-  const [state, _] = useGlobalState();
-  if (state.feature_name === "dev") return <ModulesTabs address={address} />;
-  return (
-    <Box marginTop={4}>
-      <ViewCode address={address} />
-    </Box>
-  );
+  return <ModulesTabs address={address} />;
 }
