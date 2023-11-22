67d237efb0e7b87ac64add77fcfb909648c5f01b
mvmt-ninja <jack@movementlabs.xyz>
2023-11-23 00:45:09 +0900

    feat: remove network info and adjust border style

--- src/pages/Analytics/NetworkInfo/NetworkInfo.tsx
+++ src/pages/Analytics/NetworkInfo/NetworkInfo.tsx
@@ -1,18 +1,20 @@
 import React, {createContext} from "react";
 import Grid from "@mui/material/Grid";
+/*
 import TotalSupply from "./TotalSupply";
 import TotalStake from "./TotalStake";
 import TPS from "./TPS";
 import ActiveValidators from "./ActiveValidators";
 import TotalTransactions from "./TotalTransactions";
-import {useGetInMainnet} from "../../../api/hooks/useGetInMainnet";
-import {Link} from "../../../routing";
+import { useGetInMainnet } from "../../../api/hooks/useGetInMainnet";
+import { Link } from "../../../routing";
+*/
 
 type CardStyle = "default" | "outline";
 
 export const StyleContext = createContext<CardStyle>("default");
 
-function LinkableContainer({
+/*function LinkableContainer({
   linkToAnalyticsPage,
   children,
 }: {
@@ -28,7 +30,7 @@ function LinkableContainer({
   ) : (
     <>{children}</>
   );
-}
+}*/
 
 type NetworkInfoProps = {
   isOnHomePage?: boolean;
@@ -45,31 +47,33 @@ export default function NetworkInfo({isOnHomePage}: NetworkInfoProps) {
         sx={{alignContent: "flex-start"}}
         marginBottom={onHomePage ? 6 : 0}
       >
-        {onHomePage && (
+        {/*onHomePage && (
           <Grid item xs={12} md={12} lg={12}>
             <TotalTransactions />
           </Grid>
-        )}
-        <Grid item xs={12} md={6} lg={3}>
-          <LinkableContainer linkToAnalyticsPage={onHomePage}>
-            <TotalSupply />
-          </LinkableContainer>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <LinkableContainer linkToAnalyticsPage={onHomePage}>
-            <TotalStake />
-          </LinkableContainer>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <LinkableContainer linkToAnalyticsPage={onHomePage}>
-            <TPS />
-          </LinkableContainer>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <LinkableContainer linkToAnalyticsPage={onHomePage}>
-            <ActiveValidators />
-          </LinkableContainer>
-        </Grid>
+        )*/}
+        {/*
+          <Grid item xs={12} md={6} lg={3}>
+            <LinkableContainer linkToAnalyticsPage={onHomePage}>
+              <TotalSupply />
+            </LinkableContainer>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <LinkableContainer linkToAnalyticsPage={onHomePage}>
+              <TotalStake />
+            </LinkableContainer>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <LinkableContainer linkToAnalyticsPage={onHomePage}>
+              <TPS />
+            </LinkableContainer>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <LinkableContainer linkToAnalyticsPage={onHomePage}>
+              <ActiveValidators />
+            </LinkableContainer>
+          </Grid>
+        */}
       </Grid>
     </StyleContext.Provider>
   );
