ebc4ea1e62040684850c386b15b3b82686d20594
darren wang <darren@aptoslabs.com>
2023-12-05 16:29:57 -0800

    add banner to Landing Page

--- src/pages/LandingPage/Index.tsx
+++ src/pages/LandingPage/Index.tsx
@@ -3,10 +3,12 @@ import HeaderSearch from "../layout/Search/Index";
 import Box from "@mui/material/Box";
 import NetworkInfo from "../Analytics/NetworkInfo/NetworkInfo";
 import UserTransactionsPreview from "./UserTransactionsPreview";
+import {AptosLearnBanner} from "./AptosLearnBanner";
 
 export default function LandingPage() {
   return (
     <Box>
+      <AptosLearnBanner />
       <Typography variant="h3" component="h3" marginBottom={4}>
         Aptos Explorer
       </Typography>
