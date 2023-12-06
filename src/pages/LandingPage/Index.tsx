e6bd957d162a7d96ad92f9590bdb030893868a41
darren wang <darren@aptoslabs.com>
2023-12-05 16:18:41 -0800

    delete Playground banner rip :(

--- src/pages/LandingPage/Index.tsx
+++ src/pages/LandingPage/Index.tsx
@@ -3,7 +3,6 @@ import HeaderSearch from "../layout/Search/Index";
 import Box from "@mui/material/Box";
 import NetworkInfo from "../Analytics/NetworkInfo/NetworkInfo";
 import UserTransactionsPreview from "./UserTransactionsPreview";
-import {PlaygroundBanner} from "./Components/PlaygroundBanner";
 
 export default function LandingPage() {
   return (
@@ -11,7 +10,6 @@ export default function LandingPage() {
       <Typography variant="h3" component="h3" marginBottom={4}>
         Aptos Explorer
       </Typography>
-      <PlaygroundBanner />
       <NetworkInfo isOnHomePage />
       <HeaderSearch />
       <UserTransactionsPreview />
