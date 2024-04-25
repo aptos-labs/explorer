fdd5d288b2d882034da9b36ffcfdf749e57b2470
Brian Li <brnli7@gmail.com>
2024-04-24 18:30:03 -0700

    Redirect to object page if it is not an account

--- src/api/hooks/useGetAccountResources.ts
+++ src/api/hooks/useGetAccountResources.ts
@@ -1,16 +1,20 @@
 import {Types} from "aptos";
-import {useQuery, UseQueryResult} from "@tanstack/react-query";
+import {useQuery, UseQueryOptions, UseQueryResult} from "@tanstack/react-query";
 import {getAccountResources} from "../../api";
 import {ResponseError} from "../../api/client";
 import {useGlobalState} from "../../global-config/GlobalConfig";
 
 export function useGetAccountResources(
   address: string,
+  options?: {
+    retry?: number | boolean;
+  },
 ): UseQueryResult<Types.MoveResource[], ResponseError> {
   const [state] = useGlobalState();
 
   return useQuery<Array<Types.MoveResource>, ResponseError>(
     ["accountResources", {address}, state.network_value],
     () => getAccountResources({address}, state.network_value),
+    options,
   );
 }
