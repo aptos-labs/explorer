6a7c9d45ea852bbc324034f879b58630d56a45ee
Rasa Welcher <kwelch12@gmail.com>
2024-05-07 11:49:01 -0400

    Updates react-query from 4 to 5 (#716)

--- src/pages/Account/Components/AptosNamesBanner.tsx
+++ src/pages/Account/Components/AptosNamesBanner.tsx
@@ -1,27 +1,30 @@
-import {AptosNamesConnector} from "@aptos-labs/aptos-names-connector";
 import "@aptos-labs/aptos-names-connector/dist/index.css";
 import React from "react";
 import {useGetInDevMode} from "../../../api/hooks/useGetInDevMode";
 import {Banner} from "../../../components/Banner";
-import useSubmitTransaction from "../../../api/hooks/useSubmitTransaction";
-import {useWallet} from "@aptos-labs/wallet-adapter-react";
+// import useSubmitTransaction from "../../../api/hooks/useSubmitTransaction";
+// import {useWallet} from "@aptos-labs/wallet-adapter-react";
 import {Stack, useMediaQuery, useTheme} from "@mui/material";
 
 export function AptosNamesBanner() {
   const inDev = useGetInDevMode();
-  const {connected} = useWallet();
-  const {submitTransaction} = useSubmitTransaction();
+  // const {connected} = useWallet();
+  // const {submitTransaction} = useSubmitTransaction();
   const theme = useTheme();
   const isOnMobile = !useMediaQuery(theme.breakpoints.up("md"));
 
-  const ANSConnector = (
-    <AptosNamesConnector
-      buttonLabel="Claim your name"
-      onSignTransaction={submitTransaction}
-      isWalletConnected={connected}
-      network="testnet"
-    />
-  );
+  // const ANSConnector = (
+  //   <AptosNamesConnector
+  //     buttonLabel="Claim your name"
+  //     onSignTransaction={submitTransaction}
+  //     isWalletConnected={connected}
+  //     network="testnet"
+  //   />
+  // );
+
+  // We upgraded Explorer to latest Adapter that uses @aptos-labs/ts-sdk
+  // We need to upgrade ANSConnector code also to have this functionality working
+  const ANSConnector = "to implement";
 
   const action = isOnMobile ? null : ANSConnector;
   const text = "Claim your ANS name today!";
--- src/pages/Account/Tabs/ModulesTab/Contract.tsx
+++ src/pages/Account/Tabs/ModulesTab/Contract.tsx
@@ -5,7 +5,10 @@ import {useGetAccountModules} from "../../../../api/hooks/useGetAccountModules";
 import EmptyTabContent from "../../../../components/IndividualPageContent/EmptyTabContent";
 import SidebarItem from "../../Components/SidebarItem";
 import {WalletConnector} from "@aptos-labs/wallet-adapter-mui-design";
-import {useWallet} from "@aptos-labs/wallet-adapter-react";
+import {
+  useWallet,
+  InputTransactionData,
+} from "@aptos-labs/wallet-adapter-react";
 import {
   Grid,
   Box,
@@ -278,23 +281,25 @@ function RunContractForm({
 
   const onSubmit: SubmitHandler<ContractFormType> = async (data) => {
     logEvent("write_button_clicked", fn.name);
-    const payload: Types.TransactionPayload = {
-      type: "entry_function_payload",
-      function: `${module.address}::${module.name}::${fn.name}`,
-      type_arguments: data.typeArgs,
-      arguments: data.args.map((arg, i) => {
-        const type = fnParams[i];
-        if (type.includes("vector")) {
-          // when it's a vector<u8>, we support both hex and javascript array format
-          return type === "vector<u8>" && arg.trim().startsWith("0x")
-            ? Array.from(new HexString(arg).toUint8Array()).map((x) =>
-                x.toString(),
-              )
-            : JSON.parse(arg);
-        } else if (type.startsWith("0x1::option::Option")) {
-          arg ? {vec: [arg]} : undefined;
-        } else return arg;
-      }),
+
+    const payload: InputTransactionData = {
+      data: {
+        function: `${module.address}::${module.name}::${fn.name}`,
+        typeArguments: data.typeArgs,
+        functionArguments: data.args.map((arg, i) => {
+          const type = fnParams[i];
+          if (type.includes("vector")) {
+            // when it's a vector<u8>, we support both hex and javascript array format
+            return type === "vector<u8>" && arg.trim().startsWith("0x")
+              ? Array.from(new HexString(arg).toUint8Array()).map((x) =>
+                  x.toString(),
+                )
+              : JSON.parse(arg);
+          } else if (type.startsWith("0x1::option::Option")) {
+            arg ? {vec: [arg]} : undefined;
+          } else return arg;
+        }),
+      },
     };
 
     await submitTransaction(payload);
