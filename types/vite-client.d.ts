d06a3870933e8c84555b4d7dee8f42606a995828
Primata <primata@movementlabs.xyz>
2024-06-28 12:04:51 -0300

    fix tsconfig

---
+++ types/vite-client.d.ts
@@ -0,0 +1,4 @@
+declare module "vite/client" {
+  const content: any;
+  export default content;
+}
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,12 @@
 interface ImportMetaEnv {
-    readonly VITE_APTOS_DEVNET_URL: string;
-    // add other environment variables here
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly VITE_APTOS_DEVNET_URL: string;
+  // add other environment variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+// vite-env.d.ts
+// ...
+/// <reference types="vite-plugin-svgr/client" />
