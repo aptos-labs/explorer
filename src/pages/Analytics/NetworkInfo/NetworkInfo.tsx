18ce8af79c8d3121376104136af3a80b93b02359
Vinay Pallegar <vp@ipalmedia.com>
2024-09-23 13:58:32 -0700

    add a card style for total tx and display on analytics page.

--- src/pages/Analytics/NetworkInfo/NetworkInfo.tsx
+++ src/pages/Analytics/NetworkInfo/NetworkInfo.tsx
@@ -1,36 +1,34 @@
 import React, {createContext} from "react";
 import Grid from "@mui/material/Grid";
-/*
-import TotalSupply from "./TotalSupply";
-import TotalStake from "./TotalStake";
-import TPS from "./TPS";
-import ActiveValidators from "./ActiveValidators";
+// import TotalSupply from "./TotalSupply";
+// import TotalStake from "./TotalStake";
+// import TPS from "./TPS";
+// import ActiveValidators from "./ActiveValidators";
 import TotalTransactions from "./TotalTransactions";
-import { useGetInMainnet } from "../../../api/hooks/useGetInMainnet";
-import { Link } from "../../../routing";
-*/
+// import { useGetInMainnet } from "../../../api/hooks/useGetInMainnet";
+// import { Link } from "../../../routing";
 
 type CardStyle = "default" | "outline";
 
 export const StyleContext = createContext<CardStyle>("default");
 
-/*function LinkableContainer({
-  linkToAnalyticsPage,
-  children,
-}: {
-  linkToAnalyticsPage: boolean;
-  children: React.ReactNode;
-}) {
-  const inMainnet = useGetInMainnet();
+// function LinkableContainer({
+//   linkToAnalyticsPage,
+//   children,
+// }: {
+//   linkToAnalyticsPage: boolean;
+//   children: React.ReactNode;
+// }) {
+//   const inMainnet = useGetInMainnet();
 
-  return inMainnet && linkToAnalyticsPage ? (
-    <Link to="/analytics" underline="none" color="inherit" variant="inherit">
-      {children}
-    </Link>
-  ) : (
-    <>{children}</>
-  );
-}*/
+//   return inMainnet && linkToAnalyticsPage ? (
+//     <Link to="/analytics" underline="none" color="inherit" variant="inherit">
+//       {children}
+//     </Link>
+//   ) : (
+//     <>{children}</>
+//   );
+// }
 
 type NetworkInfoProps = {
   isOnHomePage?: boolean;
@@ -47,25 +45,23 @@ export default function NetworkInfo({isOnHomePage}: NetworkInfoProps) {
         sx={{alignContent: "flex-start"}}
         marginBottom={onHomePage ? 6 : 0}
       >
-        {/*onHomePage && (
-          <Grid item xs={12} md={12} lg={12}>
-            <TotalTransactions />
-          </Grid>
-        )*/}
+        <Grid item xs={12} md={12} lg={12}>
+          <TotalTransactions type={isOnHomePage ? "inline" : "card"} />
+        </Grid>
         {/*
           <Grid item xs={12} md={6} lg={3}>
+          <Grid item xs={12} md={6} lg={3}>
             <LinkableContainer linkToAnalyticsPage={onHomePage}>
+              <TPS />
+            </LinkableContainer>
+          </Grid>
+          <LinkableContainer linkToAnalyticsPage={onHomePage}>
               <TotalSupply />
             </LinkableContainer>
           </Grid>
           <Grid item xs={12} md={6} lg={3}>
             <LinkableContainer linkToAnalyticsPage={onHomePage}>
               <TotalStake />
-            </LinkableContainer>
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <LinkableContainer linkToAnalyticsPage={onHomePage}>
-              <TPS />
             </LinkableContainer>
           </Grid>
           <Grid item xs={12} md={6} lg={3}>
