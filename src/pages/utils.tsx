9a103e17ae5bdd9fb207b722cdb3880f3197a1c7
Greg Nazario <greg@gnazar.io>
2024-10-08 08:11:38 -0700

    [txn] Fix really large timestamp (#812)

--- src/pages/utils.tsx
+++ src/pages/utils.tsx
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-function ensureMillisecondTimestamp(timestamp: string): number {
+function ensureMillisecondTimestamp(timestamp: string): bigint {
   /*
   Could be: 1646458457
         or: 1646440953658538
@@ -11,24 +11,47 @@ function ensureMillisecondTimestamp(timestamp: string): number {
   if (timestamp.length == 10) {
     timestamp = timestamp + "000";
   }
-  return parseInt(timestamp);
+  return BigInt(timestamp);
 }
 
 export function parseTimestamp(
   timestamp: string,
   ensureMilliSeconds: boolean = true,
 ): moment.Moment {
+  let time: bigint;
   if (ensureMilliSeconds) {
-    return moment(ensureMillisecondTimestamp(timestamp));
+    time = ensureMillisecondTimestamp(timestamp);
   } else {
-    return moment(parseInt(timestamp));
+    time = BigInt(timestamp);
+  }
+  if (time > 8640000000000000n) {
+    return moment(8640000000000000);
+  } else {
+    return moment(parseInt(time.toString()));
+  }
+}
+
+export function parseTimestampString(
+  timestamp: string,
+  ensureMilliSeconds: boolean = true,
+): string {
+  let time: bigint;
+  if (ensureMilliSeconds) {
+    time = ensureMillisecondTimestamp(timestamp);
+  } else {
+    time = BigInt(timestamp);
+  }
+  if (time > 8640000000000000n) {
+    return `> ${timestampDisplay(moment(8640000000000000)).local_formatted}`;
+  } else {
+    return timestampDisplay(moment(parseInt(time.toString()))).local_formatted;
   }
 }
 
 // expiration_timestamp can be user inputted so we don't want to do any ensuring of milliseconds
 // but it comes back at a different factor than what we need for parsing on the frontend
-export function parseExpirationTimestamp(timestamp: string) {
-  return timestamp + "000";
+export function parseExpirationTimestamp(timestamp: string): string {
+  return (BigInt(timestamp) * 1000n).toString(10);
 }
 
 export interface TimestampDisplay {
@@ -106,22 +129,10 @@ export function isNumeric(text: string) {
   return /^-?\d+$/.test(text);
 }
 
-export function getFormattedTimestamp(timestamp?: string): string {
-  if (!timestamp || timestamp === "0") return "-";
-
-  const moment = parseTimestamp(timestamp);
-  const timestamp_display = timestampDisplay(moment);
-
-  return timestamp_display.local_formatted;
-}
-
 export function getTableFormattedTimestamp(timestamp?: string): string {
   if (!timestamp || timestamp === "0") return "-";
 
-  const moment = parseTimestamp(timestamp);
-  const timestamp_display = timestampDisplay(moment);
-
-  return timestamp_display.local_formatted;
+  return parseTimestampString(timestamp);
 }
 
 export function isValidUrl(url: string): boolean {
