3e9e8162dedd22c741400e18da9cc2b61bcd1a80
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-10-10 12:16:14 -0700

    display package metadata

--- src/api/hooks/useGetAccountResource.ts
+++ src/api/hooks/useGetAccountResource.ts
@@ -10,9 +10,24 @@ export type ModuleMetadata = {
   source: string;
 };
 
+export type UpgradePolicy = {
+  // 0 is arbitrary, i.e. publisher can upgrade anyway they want, they need to migrate the data manually
+  // 1 is compatible
+  // 2 is immutable
+  policy: string;
+};
+
 export type PackageMetadata = {
   name: string;
   modules: ModuleMetadata[];
+  upgrade_policy: UpgradePolicy;
+  // The numbers of times this module has been upgraded. Also serves as the on-chain version.
+  upgrade_number: string;
+  // The source digest of the sources in the package. This is constructed by first building the
+  // sha256 of each individual source, than sorting them alphabetically, and sha256 them again.
+  source_digest: string;
+  // Move.toml file
+  manifest: string;
 };
 
 export function useGetAccountResource(
@@ -42,7 +57,14 @@ export function useGetAccountPackages(address: string) {
   const packages: PackageMetadata[] =
     registryData?.packages?.map((pkg): PackageMetadata => {
       const sortedModules = orderBy(pkg.modules, "name");
-      return {name: pkg.name, modules: sortedModules};
+      return {
+        name: pkg.name,
+        modules: sortedModules,
+        upgrade_policy: pkg.upgrade_policy,
+        upgrade_number: pkg.upgrade_number,
+        source_digest: pkg.source_digest,
+        manifest: pkg.manifest,
+      };
     }) || [];
 
   return orderBy(packages, "name");
