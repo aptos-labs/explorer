2f028d63010181bf1ddd4488a88aae2472484790
Greg Nazario <greg@gnazar.io>
2024-10-18 13:47:58 -0400

    [account] Show if the authentication key is rotated

--- src/pages/Account/Tabs/InfoTab.tsx
+++ src/pages/Account/Tabs/InfoTab.tsx
@@ -5,16 +5,22 @@ import ContentBox from "../../../components/IndividualPageContent/ContentBox";
 import ContentRow from "../../../components/IndividualPageContent/ContentRow";
 import EmptyTabContent from "../../../components/IndividualPageContent/EmptyTabContent";
 import {getLearnMoreTooltip} from "../../Transaction/helpers";
+import {AccountAddress} from "@aptos-labs/ts-sdk";
+import {grey} from "../../../themes/colors/aptosColorPalette";
 
 type InfoTabProps = {
   address: string;
   accountData: Types.AccountData | Types.MoveResource[] | undefined;
 };
 
-export default function InfoTab({accountData}: InfoTabProps) {
+export default function InfoTab({address, accountData}: InfoTabProps) {
   if (!accountData || Array.isArray(accountData)) {
     return <EmptyTabContent />;
   }
+
+  const keyRotated = !AccountAddress.from(address).equals(
+    AccountAddress.from(accountData.authentication_key),
+  );
 
   return (
     <Box marginBottom={3}>
@@ -24,11 +30,24 @@ export default function InfoTab({accountData}: InfoTabProps) {
           value={accountData.sequence_number}
           tooltip={getLearnMoreTooltip("sequence_number")}
         />
-        <ContentRow
-          title={"Authentication Key:"}
-          value={accountData.authentication_key}
-          tooltip={getLearnMoreTooltip("authentication_key")}
-        />
+        {keyRotated ? (
+          <ContentRow
+            title={"Authentication Key:"}
+            value={
+              <>
+                {`${accountData.authentication_key} `}
+                <span style={{marginLeft: 8, color: grey[450]}}>(rotated)</span>
+              </>
+            }
+            tooltip={getLearnMoreTooltip("authentication_key")}
+          />
+        ) : (
+          <ContentRow
+            title={"Authentication Key:"}
+            value={accountData.authentication_key}
+            tooltip={getLearnMoreTooltip("authentication_key")}
+          />
+        )}
       </ContentBox>
     </Box>
   );
