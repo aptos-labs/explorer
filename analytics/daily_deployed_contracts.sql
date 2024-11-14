652df02fdbd5e580a81aa224105157bcd00e898c
Ying <ying-w@users.noreply.github.com>
2024-11-14 07:24:05 -0800

    Optimize analytics sql (#914)
    * optimize
    
    * bonus: apt supply

--- analytics/daily_deployed_contracts.sql
+++ analytics/daily_deployed_contracts.sql
@@ -1,8 +1,10 @@
 SELECT
-  DATE(t.block_timestamp) AS day,
-  COUNT(DISTINCT t.tx_version ||'-' || t.change_index ) AS daily_contract_deployed
+  DATE(t.block_timestamp) AS ds,
+  COUNT(DISTINCT t.tx_version || '-' || t.change_index) AS daily_contract_deployed
 FROM `bigquery-public-data.crypto_aptos_mainnet_us.changes` AS t
 WHERE t.change_type= 'write_module'
-  AND DATE(t.block_timestamp) BETWEEN CURRENT_DATE -30 AND CURRENT_DATE -1
+AND t.block_timestamp BETWEEN
+  TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY) - INTERVAL 30 DAY
+  AND TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY) - INTERVAL 1 DAY
 GROUP BY 1
 ORDER BY 1
--- analytics/mau_signers.sql
+++ analytics/mau_signers.sql
@@ -1,5 +1,5 @@
 SELECT
-  ds_utc as ds,
+  ds_utc as date,
   COUNT(DISTINCT
     CASE
       WHEN
@@ -9,28 +9,35 @@ SELECT
     END
   ) AS mau_signer_30
 FROM (
-SELECT *
-FROM
-  UNNEST(
-    GENERATE_DATE_ARRAY(
-      CURRENT_DATE()-30,
-      CURRENT_DATE()-1,
-      INTERVAL 1 DAY
-    )
-  ) AS ds_utc
-) d
-CROSS JOIN (
   -- similar logic to daily_active_users 
-  SELECT
+  SELECT DISTINCT
     DATE(s.block_timestamp) as ds,
     signer
   FROM `bigquery-public-data.crypto_aptos_mainnet_us.signatures` s
   INNER JOIN `bigquery-public-data.crypto_aptos_mainnet_us.transactions` t
   USING (tx_version)
-  WHERE DATE(s.block_timestamp) BETWEEN CURRENT_DATE -61 AND CURRENT_DATE -1
+  WHERE 1=1
+  -- using block_height saves very little time (and adds another col to scan)
+  AND s.block_timestamp BETWEEN
+    TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY) - INTERVAL 61 DAY
+    AND TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY) - INTERVAL 1 DAY
+  AND t.block_timestamp BETWEEN
+    TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY) - INTERVAL 61 DAY
+    AND TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY) - INTERVAL 1 DAY
   AND t.success
   AND (NOT is_fee_payer OR is_fee_payer IS NULL)
 ) a
+CROSS JOIN (
+  SELECT *
+  FROM
+    UNNEST(
+      GENERATE_DATE_ARRAY(
+        CURRENT_DATE()-30,
+        CURRENT_DATE()-1,
+        INTERVAL 1 DAY
+      )
+    ) AS ds_utc
+) d
 WHERE ds BETWEEN ds_utc -30 AND ds_utc
 GROUP BY 1
 ORDER BY 1
