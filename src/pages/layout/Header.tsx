4f165de08dfcc79cbd3d63f168c9ad8f2f27bfc9
ekanem david <ekanemd@live.com>
2024-12-04 12:46:34 +0000

    removed networkselect

--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -2,7 +2,7 @@ import React, {useRef} from "react";
 import Toolbar from "@mui/material/Toolbar";
 import MuiAppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
-import NetworkSelect from "./NetworkSelect";
+// import NetworkSelect from "./NetworkSelect";
 // import {useColorMode} from "../../context";
 import {useMediaQuery, useTheme} from "@mui/material";
 
@@ -81,7 +81,7 @@ export default function Header() {
             gap: 2,
           }}
         >
-          <NetworkSelect />
+          {/* <NetworkSelect /> */}
           <NavMobile />
         </Box>
       </Box>
