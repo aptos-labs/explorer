46cc45d0c02e1b277f9195d8dc2fbf4fd689b14c
Greg Nazario <greg@gnazar.io>
2025-03-18 15:57:06 -0400

    [wallets] Remove legacy wallets
    Removes:
    - Rise
    - Martian
    - Token Pocket
    - Blocto

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -11,16 +11,9 @@ import {
 } from "../../global-config/GlobalConfig";
 import {ProvideColorMode} from "../../context";
 import {GraphqlClientProvider} from "../../api/hooks/useGraphqlClient";
-import {
-  AptosWalletAdapterProvider,
-  NetworkName,
-} from "@aptos-labs/wallet-adapter-react";
+import {AptosWalletAdapterProvider} from "@aptos-labs/wallet-adapter-react";
 import {BitgetWallet} from "@bitget-wallet/aptos-wallet-adapter";
-import {BloctoWallet} from "@blocto/aptos-wallet-adapter-plugin";
-import {MartianWallet} from "@martianwallet/aptos-wallet-adapter";
 import {PontemWallet} from "@pontem/wallet-adapter-plugin";
-import {RiseWallet} from "@rise-wallet/wallet-adapter";
-import {TokenPocketWallet} from "@tp-lab/aptos-wallet-adapter";
 import {TrustWallet} from "@trustwallet/aptos-wallet-adapter";
 import {WelldoneWallet} from "@welldone-studio/aptos-wallet-adapter";
 import {MSafeWalletAdapter} from "@msafe/aptos-wallet-adapter";
@@ -32,13 +25,10 @@ import {Network} from "@aptos-labs/ts-sdk";
 const AptosConnectId = "99d260d0-c69d-4c15-965f-f6f9b7b00102";
 
 // Statically initialize wallets that don't change for the network
-const martianWallet = new MartianWallet();
 const msafeWallet = new MSafeWalletAdapter();
 const rimowallet = new RimoWallet();
 const okxWallet = new OKXWallet();
 const pontemWallet = new PontemWallet();
-const riseWallet = new RiseWallet();
-const tokenPocketWallet = new TokenPocketWallet();
 const trustWallet = new TrustWallet();
 const welldoneWallet = new WelldoneWallet();
 const bitgetWallet = new BitgetWallet();
@@ -53,15 +43,7 @@ function ExplorerWalletAdapterProvider({children}: LayoutProps) {
       bitgetWallet,
       msafeWallet,
       trustWallet,
-      tokenPocketWallet,
-      martianWallet,
-      // Blocto supports Testnet/Mainnet for now.
-      new BloctoWallet({
-        network: NetworkName.Testnet,
-        bloctoAppId: "6d85f56e-5f2e-46cd-b5f2-5cf9695b4d46",
-      }),
       welldoneWallet,
-      riseWallet,
     ],
     [],
   );
