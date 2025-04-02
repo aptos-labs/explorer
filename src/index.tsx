84297b7cc6a04bcbfd4b8a60de178ef06d10425b
Greg Nazario <greg@gnazar.io>
2025-04-02 17:37:18 -0400

    [polyfill] Remove polyfills
    For a certain range of Typescript SDKs, polyfills were required.
    
    1.38.0 ensures that this doesn't break

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import "./polyfills";
 import React from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
--- src/polyfills.ts
+++
@@ -1,4 +0,0 @@
-import {Buffer} from "buffer";
-
-window.Buffer = Buffer;
-window.global = window;
