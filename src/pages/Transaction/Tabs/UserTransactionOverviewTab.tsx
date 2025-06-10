56214b9715a79dbec41b0b91a1840bcc4e6a2968
ivanfong01 <ivanfong01@gmail.com>
2025-06-10 13:28:06 +0800

    [UserTransactionOverviewTab] add support to hyperion swap event V3

--- src/pages/Transaction/Tabs/UserTransactionOverviewTab.tsx
+++ src/pages/Transaction/Tabs/UserTransactionOverviewTab.tsx
@@ -1269,7 +1269,9 @@ function parseHyperionSwapEvent(event: Types.Event): Swap | undefined {
     event.type !==
       "0x8b4a2c4bb53857c718a04c020b98f8c2e1f99a68b0f57389a8bf5434cd22e05c::pool_v3::SwapEvent" &&
     event.type !==
-      "0x8b4a2c4bb53857c718a04c020b98f8c2e1f99a68b0f57389a8bf5434cd22e05c::pool_v3::SwapEventV2"
+      "0x8b4a2c4bb53857c718a04c020b98f8c2e1f99a68b0f57389a8bf5434cd22e05c::pool_v3::SwapEventV2" &&
+    event.type !==
+    "0x8b4a2c4bb53857c718a04c020b98f8c2e1f99a68b0f57389a8bf5434cd22e05c::pool_v3::SwapEventV3"
   ) {
     return undefined;
   }
