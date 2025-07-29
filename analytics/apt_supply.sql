b8a2668562c124078373cbbd396efbdff8af6603
Ying <ying-w@users.noreply.github.com>
2025-07-29 15:22:59 -0700

    Update apt_supply.sql

--- analytics/apt_supply.sql
+++ analytics/apt_supply.sql
@@ -1,8 +1,13 @@
--- supply changes on almost every version because gas is burned
--- should give same results as view function
--- apt mint comes from validator inflation and refunded on delete
--- apt burn comes from gas fees
+-- supply changes on (almost) every version because gas is burned
+-- adding coin and FA supply together should give same answer as 0x1::coin::supply view function with "0x1::aptos_coin::AptosCoin"
+-- apt mint: validator and storage refund from delete
+-- apt burn: gas fees
+-- more details in 'APT mints and burns' section of https://medium.com/p/535e312946ad
+
 SELECT
+SUM(supply)/1e8 AS apt_supply
+FROM (
+SELECT -- coins
     tx_version,
     block_height,
     block_timestamp,
@@ -12,11 +17,12 @@ FROM `bigquery-public-data.crypto_aptos_mainnet_us.table_items`
 WHERE 1=1 -- same for mainnet and testnet
 AND address = '0x1b854694ae746cdbd8d44186ca4929b2b337df21d1c74633be19b2710552fdca' -- should be table_handle
 AND '0x'|| LPAD(LTRIM(key.name, '0x'), 64, '0') = '0x0619dc29a0aac8fa146714058e8dd6d2d0f3bdf5f6331907bf91f3acd81e6935'
-LIMIT 1000
+AND DATE(block_timestamp) = CURRENT_DATE() -- optimization
+QUALIFY ROW_NUMBER() OVER (ORDER BY tx_version DESC) = 1 -- latest
 
 UNION ALL
 
-SELECT
+SELECT -- FA
   tx_version,
   block_height,
   block_timestamp,
@@ -25,3 +31,6 @@ SELECT
 FROM `bigquery-public-data.crypto_aptos_mainnet_us.resources` r
 WHERE type_str ='0x1::fungible_asset::ConcurrentSupply'
 AND address ='0x000000000000000000000000000000000000000000000000000000000000000a'
+AND DATE(block_timestamp) = CURRENT_DATE() -- optimization
+QUALIFY ROW_NUMBER() OVER (ORDER BY tx_version DESC) = 1 -- latest
+)
