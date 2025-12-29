75e8deeddd8fc9c7d9f2c67d523e6479fed5c0f9
Greg Nazario <greg@gnazar.io>
2025-12-29 14:46:53 -0500

    fix error on paired coin

--- src/api/hooks/useGetFaPairedCoin.ts
+++ src/api/hooks/useGetFaPairedCoin.ts
@@ -13,7 +13,7 @@ const TEXT_DECODER = new TextDecoder();
 
 export function useGetFaPairedCoin(
   address: string,
-): UseQueryResult<string | undefined, ResponseError> {
+): UseQueryResult<string | null, ResponseError> {
   const networkValue = useNetworkValue();
   const aptosClient = useAptosClient();
   const request: Types.ViewRequest = {
@@ -22,32 +22,37 @@ export function useGetFaPairedCoin(
     arguments: [address],
   };
 
-  return useQuery<string | undefined, ResponseError>({
+  return useQuery<string | null, ResponseError>({
     queryKey: ["pairedCoin", address, networkValue],
     queryFn: async () => {
-      if (isValidAccountAddress(address)) {
-        const data = await view(request, aptosClient);
-        if (data !== undefined) {
-          const mappedData = data as [
+      if (!isValidAccountAddress(address)) {
+        return null;
+      }
+
+      const data = await view(request, aptosClient);
+      if (data === undefined) {
+        return null;
+      }
+
+      const mappedData = data as [
+        {
+          vec: [
             {
-              vec: [
-                {
-                  account_address: string;
-                  module_name: string;
-                  struct_name: string;
-                },
-              ];
+              account_address: string;
+              module_name: string;
+              struct_name: string;
             },
           ];
-          const val = mappedData[0]?.vec[0];
-          if (val !== undefined && val !== null) {
-            return `${val.account_address}::${hexToUtf8(val.module_name)}::${hexToUtf8(val.struct_name)}`;
-          }
-        }
-      } else {
-        return undefined;
+        },
+      ];
+      const val = mappedData[0]?.vec[0];
+      if (val === undefined || val === null) {
+        return null;
       }
+
+      return `${val.account_address}::${hexToUtf8(val.module_name)}::${hexToUtf8(val.struct_name)}`;
     },
+    enabled: isValidAccountAddress(address),
   });
 }
 
