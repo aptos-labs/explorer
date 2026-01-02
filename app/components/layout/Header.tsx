6eaf1d23d354eecc3c5e3e75c9e8e5029d5b4d44
Greg Nazario <greg@gnazar.io>
2026-01-01 23:45:33 -0500

    [tanstack] fix links

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect, useState} from "react";
+import React, {useState} from "react";
 import Toolbar from "@mui/material/Toolbar";
 import MuiAppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
@@ -19,32 +19,14 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
 import {Link, useNavigate} from "@tanstack/react-router";
 import {useInView} from "react-intersection-observer";
-import {useNetworkName} from "../../global-config";
 import NetworkSelect from "./NetworkSelect";
-
-// Aptos Logo component
-const AptosLogoIcon = ({isDark}: {isDark: boolean}) => (
-  <svg
-    width="48"
-    height="48"
-    viewBox="0 0 36 36"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M26.8461 12.3333H21.4948C21.1102 12.3333 20.7386 12.4666 20.4408 12.7111L17.3333 15.2222L14.2259 12.7111C13.9281 12.4666 13.5565 12.3333 13.1719 12.3333H7.82051C7.08888 12.3333 6.49487 12.9273 6.49487 13.659V14.2222C6.49487 14.9539 7.08888 15.5479 7.82051 15.5479H12.3999L17.3333 19.5556L22.2668 15.5479H26.8461C27.5778 15.5479 28.1718 14.9539 28.1718 14.2222V13.659C28.1718 12.9273 27.5778 12.3333 26.8461 12.3333Z"
-      fill={isDark ? "#F9F9F0" : "#0F0E0B"}
-    />
-    <path
-      d="M26.8461 20.4521H21.4948C21.1102 20.4521 20.7386 20.5854 20.4408 20.8299L17.3333 23.341L14.2259 20.8299C13.9281 20.5854 13.5565 20.4521 13.1719 20.4521H7.82051C7.08888 20.4521 6.49487 21.0461 6.49487 21.7778V22.341C6.49487 23.0727 7.08888 23.6667 7.82051 23.6667H12.3999L17.3333 27.6744L22.2668 23.6667H26.8461C27.5778 23.6667 28.1718 23.0727 28.1718 22.341V21.7778C28.1718 21.0461 27.5778 20.4521 26.8461 20.4521Z"
-      fill={isDark ? "#F9F9F0" : "#0F0E0B"}
-    />
-  </svg>
-);
+// Import the actual Aptos logos
+import LogoIconLight from "../../assets/svg/aptos_logo_icon_light.svg?react";
+import LogoIconDark from "../../assets/svg/aptos_logo_icon_dark.svg?react";
+import IconLight from "../../assets/svg/icon_light.svg?react";
+import IconDark from "../../assets/svg/icon_dark.svg?react";
 
 const navItems = [
   {label: "Transactions", to: "/transactions"},
@@ -78,7 +60,6 @@ export default function Header() {
   });
 
   const isOnMobile = !useMediaQuery(theme.breakpoints.up("md"));
-  const networkName = useNetworkName();
   const navigate = useNavigate();
 
   return (
@@ -131,7 +112,11 @@ export default function Header() {
                 color: "inherit",
               }}
             >
-              <AptosLogoIcon isDark={isDark} />
+              {isDark ? (
+                <LogoIconDark width="3rem" height="3rem" />
+              ) : (
+                <LogoIconLight width="3rem" height="3rem" />
+              )}
               <Typography
                 variant="h6"
                 sx={{
@@ -187,7 +172,7 @@ export default function Header() {
                 "&:hover": {background: "transparent", opacity: "0.8"},
               }}
             >
-              {isDark ? <DarkModeIcon /> : <LightModeIcon />}
+              {isDark ? <IconDark /> : <IconLight />}
             </Button>
 
             {/* Mobile Menu Button */}
