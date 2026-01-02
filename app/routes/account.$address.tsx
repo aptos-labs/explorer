0d608d6464e11fc865ea485a8046ee6978236dfd
Greg Nazario <greg@gnazar.io>
2026-01-02 15:31:33 -0500

    [fix] Fixup tanstack version incompatibilty

--- app/routes/account.$address.tsx
+++ app/routes/account.$address.tsx
@@ -2,11 +2,6 @@ import {createFileRoute} from "@tanstack/react-router";
 import {BASE_URL, DEFAULT_OG_IMAGE} from "../lib/constants";
 import {truncateAddress} from "../utils";
 import AccountPage from "../pages/Account/Index";
-import {
-  accountInfoQueryOptions,
-  accountResourcesQueryOptions,
-} from "../api/queries";
-import {getClientFromSearch} from "../api/createClient";
 import {PagePending} from "../components/NavigationPending";
 
 export const Route = createFileRoute("/account/$address")({
@@ -38,23 +33,6 @@ export const Route = createFileRoute("/account/$address")({
     ],
     links: [{rel: "canonical", href: `${BASE_URL}/account/${params.address}`}],
   }),
-  // Loader prefetches account data
-  loader: async ({context, params, location}) => {
-    const search = location.search as Record<string, string | undefined>;
-    const client = getClientFromSearch(search);
-
-    // Prefetch account info and resources in parallel
-    await Promise.all([
-      context.queryClient.ensureQueryData(
-        accountInfoQueryOptions(params.address, client),
-      ),
-      context.queryClient.ensureQueryData(
-        accountResourcesQueryOptions(params.address, client),
-      ),
-    ]);
-
-    return {};
-  },
   pendingComponent: PagePending,
   component: AccountPage,
 });
