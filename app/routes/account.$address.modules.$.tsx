3e92d27832a43cc693951f9901c5dbd003b0f126
WGB5445 <919603023@qq.com>
2026-01-10 00:49:18 +0800

    Enhance account and object module pages with structured layout
    - Introduced Grid layout for better organization of components.
    - Added PageHeader, AccountTitle, and BalanceCard to both account and object module pages.
    - Updated ModulesTabs to be displayed within the new layout structure.

--- app/routes/account.$address.modules.$.tsx
+++ app/routes/account.$address.modules.$.tsx
@@ -1,5 +1,9 @@
 import {createFileRoute, useParams} from "@tanstack/react-router";
+import {Grid} from "@mui/material";
 import ModulesTabs from "../pages/Account/Tabs/ModulesTab/Tabs";
+import PageHeader from "../pages/layout/PageHeader";
+import AccountTitle from "../pages/Account/Title";
+import BalanceCard from "../pages/Account/BalanceCard";
 
 // Splat route for /account/:address/modules/*
 // Captures: /modules/code, /modules/code/MyModule, /modules/view/MyModule/myFunc, etc.
@@ -9,5 +13,20 @@ export const Route = createFileRoute("/account/$address/modules/$")({
 
 function ModulesPage() {
   const {address} = useParams({strict: false}) as {address: string};
-  return <ModulesTabs address={address} isObject={false} />;
+  return (
+    <Grid container spacing={1}>
+      <Grid size={{xs: 12, md: 12, lg: 12}}>
+        <PageHeader />
+      </Grid>
+      <Grid size={{xs: 12, md: 8, lg: 9}} alignSelf="center">
+        <AccountTitle address={address} isObject={false} />
+      </Grid>
+      <Grid size={{xs: 12, md: 4, lg: 3}} marginTop={{md: 0, xs: 2}}>
+        <BalanceCard address={address} />
+      </Grid>
+      <Grid size={{xs: 12, md: 12, lg: 12}} marginTop={4}>
+        <ModulesTabs address={address} isObject={false} />
+      </Grid>
+    </Grid>
+  );
 }
